e79518bdb41755a579f1c86076e6ff32c7e5b9dc
xincheng <xincheng213618@gmail.com>
2023-04-23 16:52:52 +0800

    添加 editorconfig  和  修改App 启动 和 添加 snk

--- ColorVision/App.xaml.cs
+++ ColorVision/App.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,38 @@ namespace ColorVision
     /// </summary>
     public partial class App : Application
     {
+        [STAThread]
+        [DebuggerNonUserCode]
+        [GeneratedCode("PresentationBuildTasks", "4.0.0.0")]
+        public static void Main(string[] args)
+        {
+            App app;
+            app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+
+        private App()
+        {
+            Startup += (s, e) => Application_Startup(s, e);
+            Exit += new ExitEventHandler(Application_Exit);
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("zh-Hans");
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        /// <summary>
+        /// Application Close
+        /// </summary>
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            Environment.Exit(0);
+        }
     }
 }
