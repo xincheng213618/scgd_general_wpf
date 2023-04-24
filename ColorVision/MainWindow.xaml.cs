ce58721e202d2cc6b51f276e9f17a82a17512cbe
xincheng <xincheng213618@gmail.com>
2023-04-24 17:52:47 +0800

    添加图像上的点

--- ColorVision/MainWindow.xaml.cs
+++ ColorVision/MainWindow.xaml.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -45,6 +48,7 @@ namespace ColorVision
                 string filePath = openFileDialog.FileName;
                 ImageShow.Source = new BitmapImage(new Uri(filePath));
 
+                ImageShow.AddVisual(drawingVisual2);
                 // 在这里处理所选文件的逻辑。
             }
         }
@@ -103,6 +107,51 @@ namespace ColorVision
             }
         }
 
+
+        public DrawingVisual drawingVisual2 = new DrawingVisual();
+
+        private void DrawImage(Point actPoint, Point bitPoint)
+        {
+            if (ImageShow.Source is BitmapImage bitmapImage)
+            {
+                if (bitPoint.X > 60 && bitPoint.X < bitmapImage.PixelWidth - 60 && bitPoint.Y > 45 && bitPoint.Y < bitmapImage.PixelHeight - 45)
+                {
+                    CroppedBitmap croppedBitmap = new CroppedBitmap(bitmapImage, new Int32Rect(bitPoint.X.ToInt32() - 60, bitPoint.Y.ToInt32() - 45, 120, 90));
+
+
+                    using DrawingContext dc = drawingVisual2.RenderOpen();
+                    dc.DrawImage(croppedBitmap, new Rect(new Point(actPoint.X, actPoint.Y+ 25), new Size(120, 90)));
+                    dc.DrawLine(new Pen(Brushes.Red, 10), new Point(1,0), new Point(1,1000));
+
+                    dc.DrawLine(new Pen(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00B1FF")), 3), new Point(actPoint.X + 59 , actPoint.Y + 25), new Point(actPoint.X + 59, actPoint.Y + 25 + 90));
+                    dc.DrawLine(new Pen(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00B1FF")), 3), new Point(actPoint.X, actPoint.Y + 25 +44), new Point(actPoint.X +120, actPoint.Y + 25 + 44));
+
+
+                    double x1 = actPoint.X;
+                    double y1 = actPoint.Y + 25;
+
+                    double width = 120;
+                    double height = 90;
+
+
+                    dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1,y1), new Point(x1,y1+height));
+                    dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1, y1), new Point(x1 + width, y1));
+                    dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1 + width, y1), new Point(x1 + width, y1 + height));
+                    dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1, y1 + height),new Point(x1 + width, y1 + height));
+
+                    x1 = x1 +1;
+                    y1 = y1 + 1;
+                    width -= 2;
+                    height -= 2;
+                    dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1), new Point(x1, y1 + height));
+                    dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1), new Point(x1 + width, y1));
+                    dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1 + width, y1), new Point(x1 + width, y1 + height));
+                    dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1 + height), new Point(x1 + width, y1 + height));
+
+                }
+            }
+        }
+
         private void ImageShow_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (sender is DrawCanvas drawCanvas)
@@ -152,6 +201,7 @@ namespace ColorVision
             if (sender is DrawCanvas drawCanvas)
             {
                 var point = e.GetPosition(drawCanvas);
+
                 var controlWidth = drawCanvas.ActualWidth;
                 var controlHeight = drawCanvas.ActualHeight;
 
@@ -160,8 +210,17 @@ namespace ColorVision
                 {
                     int imageWidth =bitmapImage.PixelWidth;
                     int imageHeight = bitmapImage.PixelHeight;
+
+                    var actPoint = new Point(point.X, point.Y);
+
+
                     point.X = point.X / controlWidth * imageWidth;
                     point.Y = point.Y / controlHeight * imageHeight;
+
+                    var bitPoint = new Point(point.X.ToInt32(), point.Y.ToInt32());
+
+                    DrawImage(actPoint, bitPoint);
+
 
                     if (point.X.ToInt32() >=0 && point.X.ToInt32() < bitmapImage.PixelWidth && point.Y.ToInt32() >= 0 && point.Y.ToInt32() < bitmapImage.PixelHeight)
                     {
@@ -170,7 +229,6 @@ namespace ColorVision
                         ImageInfo.Y = point.Y.ToInt32();
                         ImageInfo.X1 = point.X;
                         ImageInfo.Y1 = point.Y;
-
 
                         ImageInfo.R = color.R;
                         ImageInfo.G = color.G;
