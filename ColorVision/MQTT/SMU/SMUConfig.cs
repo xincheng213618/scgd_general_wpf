f317bca407a122889d459c63e0d76629128c7005
chenhong <chenhong@color-vision.com.cn>
2023-08-17 09:54:19 +0800

    源表

--- ColorVision/MQTT/SMU/SMUConfig.cs
+++ ColorVision/MQTT/SMU/SMUConfig.cs
@@ -1,5 +1,10 @@
 ﻿namespace ColorVision.MQTT.SMU
 {
+    public class SMUOpenParam
+    {
+        public bool IsNet { set; get; }
+        public string DevName { set; get; }
+    }
     public class SMUConfig : BaseDeviceConfig
     {
         private bool _IsNet;
