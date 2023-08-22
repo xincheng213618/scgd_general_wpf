b16635b9e5769f5379f56178f66800b2503549d2
xincheng <xincheng213618@gmail.com>
2023-08-22 10:25:01 +0800

    Fix

--- ColorVision/MQTT/Spectrum/SpectrumView.xaml.cs
+++ ColorVision/MQTT/Spectrum/SpectrumView.xaml.cs
@@ -103,9 +103,6 @@ namespace ColorVision.MQTT.Spectrum
             listView1.Visibility = Visibility.Collapsed;
             listView2.Visibility = Visibility.Collapsed;
 
-
-
-
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
