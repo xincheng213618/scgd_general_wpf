42bd89b565353c6438a5e2e7071bb267cd6d7f86
chenhong <chenhong@color-vision.com.cn>
2023-08-28 11:41:10 +0800

    PG

--- ColorVision/Device/PG/PGConfig.cs
+++ ColorVision/Device/PG/PGConfig.cs
@@ -4,6 +4,7 @@ namespace ColorVision.Device.PG
 {
     public class PGConfig : BaseDeviceConfig, IServiceConfig
     {
+        public string Category { get; set; }
         public bool IsTCPIP { get; set; }
         public string Addr { get; set; }
         public int Port { get; set; }
