0ee6fa726eb689dfd4499c29d7b309ce3824c237
chenhong <chenhong@color-vision.com.cn>
2023-09-12 15:04:34 +0800

    wpf包装winform

---
+++ WpfFlowControlLibrary/CVFlowControl.xaml.cs
@@ -0,0 +1,87 @@
+﻿using ST.Library.UI.NodeEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFlowControlLibrary
+{
+    /// <summary>
+    /// CVFlowControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class CVFlowControl : UserControl
+    {
+        private FlowEngineLib.FlowEngineControl flowEngine;
+        public FlowEngineLib.FlowEngineControl FlowEngineControl { get { return flowEngine; } }
+        public CVFlowControl()
+        {
+            flowEngine = new FlowEngineLib.FlowEngineControl(false);
+            InitializeComponent();
+        }
+
+        private void UserControl_Initialized(object sender, EventArgs e)
+        {
+            STNodeEditorMain.LoadAssembly("FlowEngineLib.dll");
+            STNodePropertyGrid1.IsEditEnable = false;
+            STNodeEditorMain.ActiveChanged += (s, e) =>
+            {
+                winf2.Visibility = STNodeEditorMain.ActiveNode == null ? Visibility.Collapsed : Visibility.Visible;
+                STNodePropertyGrid1.SetNode(STNodeEditorMain.ActiveNode);
+            };
+            flowEngine.AttachLoader(STNodeEditorMain);
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (this.ActualWidth > 200)
+            {
+                winf1.Height = (int)this.ActualHeight;
+                winf1.Width = (int)this.ActualWidth;
+            }
+        }
+
+        private void UserControl_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                STNodeEditorMain.MoveCanvas(STNodeEditorMain.CanvasOffsetX + 100, STNodeEditorMain.CanvasOffsetY, bAnimation: true, CanvasMoveArgs.Left);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Right)
+            {
+                STNodeEditorMain.MoveCanvas(STNodeEditorMain.CanvasOffsetX - 100, STNodeEditorMain.CanvasOffsetY, bAnimation: true, CanvasMoveArgs.Left);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Up)
+            {
+                STNodeEditorMain.MoveCanvas(STNodeEditorMain.CanvasOffsetX, STNodeEditorMain.CanvasOffsetY + 100, bAnimation: true, CanvasMoveArgs.Top);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Down)
+            {
+                STNodeEditorMain.MoveCanvas(STNodeEditorMain.CanvasOffsetX, STNodeEditorMain.CanvasOffsetY - 100, bAnimation: true, CanvasMoveArgs.Top);
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Add)
+            {
+                STNodeEditorMain.ScaleCanvas(STNodeEditorMain.CanvasScale + 0.1f, (STNodeEditorMain.Width / 2), (STNodeEditorMain.Height / 2));
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Subtract)
+            {
+                STNodeEditorMain.ScaleCanvas(STNodeEditorMain.CanvasScale - 0.1f, (STNodeEditorMain.Width / 2), (STNodeEditorMain.Height / 2));
+                e.Handled = true;
+            }
+        }
+    }
+}
