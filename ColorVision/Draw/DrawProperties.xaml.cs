97cc2eb1a06b7ccac77a3729398faf94971d48bd
xincheng <xincheng213618@gmail.com>
2023-09-20 11:37:51 +0800

    update

--- ColorVision/Draw/DrawProperties.xaml.cs
+++ ColorVision/Draw/DrawProperties.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#pragma warning disable CS8604, CS8605
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -28,18 +29,44 @@ namespace ColorVision.Draw
 
         private void Window_Initialized(object sender, EventArgs e)
         {
-            ComboBoxBrush.ItemsSource = typeof(Brushes).GetProperties();
+            ComboBoxBrush.ItemsSource = from Brushes in typeof(Brushes).GetProperties()
+                                        select new KeyValuePair<Brush, string>((Brush)Brushes.GetValue(null), Brushes.Name);
+            ComboBoxBrush.SelectedValuePath = "Key";
+            ComboBoxBrush.DisplayMemberPath = "Value";
+
             ComboBoxFontFamily.ItemsSource = from FontFamily in Fonts.SystemFontFamilies
                                              select new KeyValuePair<FontFamily, string>(FontFamily, FontFamily.FamilyNames.TryGetValue(XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.Name), out string fontName)? fontName: FontFamily.ToString());
             ComboBoxFontFamily.SelectedValuePath = "Key";
+
+            ComboBoxFontWeight.ItemsSource = from FontWeight in typeof(FontWeights).GetProperties()
+                                             select new KeyValuePair<FontWeight, string>((FontWeight)FontWeight.GetValue(null), FontWeight.Name);
+            ComboBoxFontWeight.SelectedValuePath = "Key";
+
+            ComboBoxFontStyle.ItemsSource = from FontStyle in typeof(FontStyles).GetProperties()
+                                            select new KeyValuePair<FontStyle, string>((FontStyle)FontStyle.GetValue(null), FontStyle.Name);
+            ComboBoxFontStyle.SelectedValuePath = "Key";
+
+
+            ComboBoxFlowDirection.ItemsSource = from FlowDirection in Enum.GetValues(typeof(FlowDirection)).Cast<FlowDirection>()
+                                                select new KeyValuePair<FlowDirection, string>(FlowDirection,FlowDirection.ToString());
+            ComboBoxFlowDirection.SelectedValuePath = "Key";
+
+            ComboBoxFontStretch.ItemsSource = from FontStretch in typeof(FontStretches).GetProperties()
+                                               select new KeyValuePair<FontStretch, string>((FontStretch)FontStretch.GetValue(null), FontStretch.Name);
+            ComboBoxFontStretch.SelectedValuePath = "Key";
+
+
+
             StackPanelTextAttribute.DataContext = DefalutTextAttribute.Defalut;
+
+
+
         }
 
         private void ComboBoxBrush_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {            
             if (ComboBoxBrush.SelectedIndex > -1)
             {
-                MessageBox.Show("!");
             }
         }
 
@@ -49,6 +76,21 @@ namespace ColorVision.Draw
             {
                 //MessageBox.Show("!");
             }
+        }
+
+        private void ComboBoxFontStyle_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxFlowDirection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxFontStretch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
