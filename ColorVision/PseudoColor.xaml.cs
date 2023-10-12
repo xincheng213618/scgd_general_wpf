8cbc7808335eb72914e7ceda481e0ffe6454a849
sjh63 <sjh63@COLORVISION>
2023-10-12 15:47:16 +0800

    up

--- ColorVision/PseudoColor.xaml.cs
+++ ColorVision/PseudoColor.xaml.cs
@@ -24,5 +24,10 @@ namespace ColorVision
         {
             InitializeComponent();
         }
+
+        private void button_Create_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
