e4e2d97dc17ee68f1224e90f68e786b9664e3d89
xincheng <xincheng213618@gmail.com>
2023-11-15 15:56:28 +0800

    样式调整

--- ColorVision/Templates/Algorithm/FocusPointsParam.cs
+++ ColorVision/Templates/Algorithm/FocusPointsParam.cs
@@ -14,27 +14,6 @@ namespace ColorVision.Templates.Algorithm
         {
         }
 
-        [Category("FocusPoints"), Description("MTF dRatio")]
-        public double MTF_dRatio { get => GetValue(_MTF_dRatio); set { SetProperty(ref _MTF_dRatio, value); } }
-        private double _MTF_dRatio = 0.01;
-
-
-        [Category("FocusPoints"), Description("EvaFunc")]
-        public EvaFunc eEvaFunc { get => GetValue(_eEvaFunc); set { SetProperty(ref _eEvaFunc, value); } }
-        private EvaFunc _eEvaFunc = EvaFunc.CalResol;
-
-
-        [Category("FocusPoints"), Description("dx")]
-        public int dx { get => GetValue(_dx); set { SetProperty(ref _dx, value); } }
-        private int _dx ;
-
-        [Category("FocusPoints"), Description("dy")]
-        public int dy { get => GetValue(_dy); set { SetProperty(ref _dy, value); } }
-        private int _dy = 1;
-
-        [Category("FocusPoints"), Description("dx")]
-        public int ksize { get => GetValue(_ksize); set { SetProperty(ref _ksize, value); } }
-        private int _ksize = 5;
 
     }
 }
