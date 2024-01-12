036b0294d6d4063a70641482f918925a54154b0f
xincheng <xincheng213618@gmail.com>
2024-01-12 11:08:41 +0800

    update

--- ColorVision/Services/Device/Camera/ServiceCamera.cs
+++ ColorVision/Services/Device/Camera/ServiceCamera.cs
@@ -86,7 +86,7 @@ namespace ColorVision.Device.Camera
         }
         public MsgRecord GetAllDevice()
         {
-            //PublishAsyncClient(new MsgSend { EventName = "CM_GetAllSnID" });
+            PublishAsyncClient(new MsgSend { EventName = "CM_GetAllSnID" });
         }
     }
 }
