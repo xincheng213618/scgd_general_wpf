a0c98e0b5b2323359a3f1a3f735fcbda78f7e685
xincheng <xincheng213618@gmail.com>
2024-02-04 14:57:24 +0800

    update

--- ColorVision/Services/Devices/Spectrum/SpectrumResourceControl.xaml.cs
+++ ColorVision/Services/Devices/Spectrum/SpectrumResourceControl.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using ColorVision.Services.Devices.Camera;
+using ColorVision.Services.Devices.Camera.Calibrations;
+using ColorVision.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,18 +45,44 @@ namespace ColorVision.Services.Devices.Spectrum
         private void UserControl_Initialized(object sender, EventArgs e)
         {
             this.DataContext = SpectrumResourceParam;
+            ComboBoxList.ItemsSource = BaseResources;
+
         }
+        public ObservableCollection<BaseResource> BaseResources { get; set; } = new ObservableCollection<BaseResource>();
 
         public void Initializedsss(DeviceSpectrum deviceSpectrum, SpectrumResourceParam spectrumResourceParam)
         {
             DeviceSpectrum = deviceSpectrum;
             SpectrumResourceParam = spectrumResourceParam;
             this.DataContext = SpectrumResourceParam;
+
+            string CalibrationMode = SpectrumResourceParam.ResourceMode;
+            ComboBoxList.SelectionChanged -= ComboBox_SelectionChanged;
+            BaseResources.Clear();
+            foreach (var item in deviceSpectrum.VisualChildren)
+            {
+                if (item is BaseResource baseResources)
+                {
+                    BaseResources.Add(baseResources);
+                }
+            }
+            ComboBoxList.Text = CalibrationMode;
+            ComboBoxList.SelectionChanged += ComboBox_SelectionChanged;
+
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (sender is ComboBox comboBox)
+            {
+                SpectrumResourceParam.IsSelected = false;
 
+                if (comboBox.SelectedValue is BaseResource baseResource)
+                {
+                    SpectrumResourceParam.ResourceName = baseResource.Name;
+                    SpectrumResourceParam.ResourceId = baseResource.Id;
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
