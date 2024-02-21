0290ab5483c3160f462cd411c7dfaa4bca965d8d
chenhong <chenhong@color-vision.com.cn>
2024-02-21 14:36:17 +0800

    状态更新

--- ColorVision/Services/RC/MQTTRCService.cs
+++ ColorVision/Services/RC/MQTTRCService.cs
@@ -4,6 +4,7 @@ using ColorVision.Services;
 using ColorVision.Services.Devices;
 using ColorVision.Services.Devices.Camera;
 using ColorVision.Settings;
+using log4net;
 using MQTTMessageLib;
 using MQTTMessageLib.RC;
 using MQTTMessageLib.Util;
@@ -37,6 +38,7 @@ namespace ColorVision.RC
     /// </summary>
     public class MQTTRCService : MQTTServiceBase
     {
+        private static readonly log4net.ILog logger = LogManager.GetLogger(typeof(MQTTRCService));
         private static MQTTRCService _instance;
         private static readonly object _locker = new();
         public static MQTTRCService GetInstance() { lock (_locker) { return _instance ??= new MQTTRCService(); } }
@@ -156,8 +158,42 @@ namespace ColorVision.RC
             }
             return Task.CompletedTask;
         }
+        public static void UpdateServiceStatus(List<MQTTNodeServiceStatus> services)
+        {
+            var deves = ServiceManager.GetInstance().DeviceServices.ToList();
+            foreach (var svr in services)
+            {
+                foreach (var devNew in svr.DeviceList)
+                {
+                    foreach (var dev in deves)
+                    {
+                        if (dev is DeviceService baseChannel && baseChannel.GetConfig() is DeviceServiceConfig baseDeviceConfig)
+                        {
+                            if (devNew.Code == baseDeviceConfig.Code)
+                            {
+                                if (!string.IsNullOrEmpty(svr.LiveTime))
+                                {
+                                    DateTime lastLive;
+                                    if (DateTime.TryParse(svr.LiveTime, out lastLive))
+                                    {
+                                        baseDeviceConfig.IsAlive = true;
+                                        baseDeviceConfig.LastAliveTime = lastLive;
+                                        baseDeviceConfig.DeviceStatus = (DeviceStatusType)Enum.Parse(typeof(DeviceStatusType), devNew.Status);
+                                        if (dev is DeviceCamera deviceCamera)
+                                        {
+                                            deviceCamera.DeviceService.DeviceStatus = baseDeviceConfig.DeviceStatus;
+                                        }
+                                    }
+                                }
 
-        public static void UpdateServiceStatus(List<MQTTNodeServiceStatus> data)
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public static void UpdateServiceStatusOld(List<MQTTNodeServiceStatus> data)
         {
             foreach (var serviceKind in ServiceManager.GetInstance().TypeServices.ToList())
             {
@@ -169,8 +205,8 @@ namespace ColorVision.RC
                         {
                             if (ss.ServiceType.ToLower(CultureInfo.CurrentCulture) == serviceKind.ServiceTypes.ToString().ToLower(CultureInfo.CurrentCulture) && ss.ServiceName == serviceTerminal.Code)
                             {
-                                serviceTerminal.Config.IsAlive = true;
-                                serviceTerminal.Config.LastAliveTime = DateTime.Now;
+                                //serviceTerminal.Config.IsAlive = true;
+                                //serviceTerminal.Config.LastAliveTime = DateTime.Now;
                                 foreach (var devNew in ss.DeviceList)
                                 {
                                     foreach (var dev in serviceTerminal.VisualChildren)
@@ -179,13 +215,21 @@ namespace ColorVision.RC
                                         {
                                             if (devNew.Code == baseDeviceConfig.Code)
                                             {
-                                                baseDeviceConfig.IsAlive = true;
-                                                baseDeviceConfig.LastAliveTime = DateTime.Parse(ss.LiveTime);
-                                                baseDeviceConfig.DeviceStatus = (DeviceStatusType)Enum.Parse(typeof(DeviceStatusType), devNew.Status);
-                                                if (dev is DeviceCamera deviceCamera)
+                                                if (!string.IsNullOrEmpty(ss.LiveTime))
                                                 {
-                                                    deviceCamera.DeviceService.DeviceStatus = baseDeviceConfig.DeviceStatus;
+                                                    DateTime lastLive;
+                                                    if(DateTime.TryParse(ss.LiveTime,out lastLive))
+                                                    {
+                                                        baseDeviceConfig.IsAlive = true;
+                                                        baseDeviceConfig.LastAliveTime = lastLive;
+                                                        baseDeviceConfig.DeviceStatus = (DeviceStatusType)Enum.Parse(typeof(DeviceStatusType), devNew.Status);
+                                                        if (dev is DeviceCamera deviceCamera)
+                                                        {
+                                                            deviceCamera.DeviceService.DeviceStatus = baseDeviceConfig.DeviceStatus;
+                                                        }
+                                                    }
                                                 }
+   
                                                 break;
                                             }
                                         }
@@ -247,10 +291,10 @@ namespace ColorVision.RC
                         var nodeService = GetNodeService(itemService.Value, serviceTerminal.Code);
                         if (nodeService == null) { continue; }
                         tokens[nodeService.ServiceName] = nodeService.ServiceToken;
-                        DateTime dateTime;
-                        if (DateTime.TryParse(nodeService.LiveTime, out dateTime)) serviceTerminal.Config.LastAliveTime = dateTime;
-                        else serviceTerminal.Config.LastAliveTime = DateTime.Now;
-                        serviceTerminal.Config.IsAlive = true;
+                        //DateTime dateTime;
+                        //if (DateTime.TryParse(nodeService.LiveTime, out dateTime)) serviceTerminal.Config.LastAliveTime = dateTime;
+                        //else serviceTerminal.Config.LastAliveTime = DateTime.Now;
+                        //serviceTerminal.Config.IsAlive = true;
                         serviceTerminal.Config.SendTopic = nodeService.UpChannel;
                         serviceTerminal.Config.SubscribeTopic = nodeService.DownChannel;
                         serviceTerminal.Config.HeartbeatTime = nodeService.OverTime * 2;
@@ -263,10 +307,10 @@ namespace ColorVision.RC
                                 baseDeviceConfig.ServiceToken = nodeService.ServiceToken;
                                 baseDeviceConfig.SendTopic = nodeService.UpChannel;
                                 baseDeviceConfig.SubscribeTopic = nodeService.DownChannel;
-                                var devNew = nodeService.GetDeviceByCode(baseDeviceConfig.Code);
-                                if (devNew == null) { continue; }
-                                baseDeviceConfig.IsAlive = true;
-                                baseDeviceConfig.DeviceStatus = (DeviceStatusType)Enum.Parse(typeof(DeviceStatusType), devNew.Status.ToString());
+                                //var devNew = nodeService.GetDeviceByCode(baseDeviceConfig.Code);
+                                //if (devNew == null) { continue; }
+                                //baseDeviceConfig.IsAlive = true;
+                                //baseDeviceConfig.DeviceStatus = (DeviceStatusType)Enum.Parse(typeof(DeviceStatusType), devNew.Status.ToString());
                             }
                         }
                     }
