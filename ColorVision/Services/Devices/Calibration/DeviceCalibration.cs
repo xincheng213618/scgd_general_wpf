7ff49885a2159d882bbe39eb2167a5e85f78e596
xincheng <xincheng213618@gmail.com>
2024-02-29 16:06:48 +0800

    update

--- ColorVision/Services/Devices/Calibration/DeviceCalibration.cs
+++ ColorVision/Services/Devices/Calibration/DeviceCalibration.cs
@@ -1,4 +1,6 @@
 ﻿using ColorVision.Services.Dao;
+using ColorVision.Services.Devices.Camera;
+using System;
 using System.Windows.Controls;
 
 namespace ColorVision.Services.Devices.Calibration
@@ -10,6 +12,7 @@ namespace ColorVision.Services.Devices.Calibration
         public DeviceCalibration(SysDeviceModel sysResourceModel) : base(sysResourceModel)
         {
             DeviceService = new MQTTCalibration(Config);
+            EditLazy = new Lazy<EditCalibration>(() => { EditCalibration ??= new EditCalibration(this); return EditCalibration; });
         }
 
         public override UserControl GetDeviceControl() => new DeviceCalibrationControl(this);
@@ -17,6 +20,12 @@ namespace ColorVision.Services.Devices.Calibration
         public override UserControl GetDeviceInfo() => new DeviceCalibrationControl(this,false);
 
         public override UserControl GetDisplayControl() => new DisplayCalibrationControl(this);
+
+
+
+        readonly Lazy<EditCalibration> EditLazy;
+        public EditCalibration EditCalibration { get; set; }
+        public override UserControl GetEditControl() => EditLazy.Value;
 
 
     }
--- ColorVision/Services/Devices/Calibration/DeviceCalibrationControl.xaml.cs
+++ ColorVision/Services/Devices/Calibration/DeviceCalibrationControl.xaml.cs
@@ -9,22 +9,29 @@ namespace ColorVision.Services.Devices.Calibration
     /// </summary>
     public partial class DeviceCalibrationControl : UserControl, IDisposable
     {
-        public DeviceCalibration Device { get; set; }
+        public DeviceCalibration DeviceCalibration { get; set; }
         public ServiceManager ServiceControl { get; set; }
 
         public bool IsCanEdit { get; set; }
 
         public DeviceCalibrationControl(DeviceCalibration deviceCalibration, bool isCanEdit = true)
         {
-            this.Device = deviceCalibration;
+            this.DeviceCalibration = deviceCalibration;
             IsCanEdit = isCanEdit;
             InitializeComponent();
         }
 
         private void UserControl_Initialized(object sender, EventArgs e)
         {
+            this.DataContext = this.DeviceCalibration;
             if (!IsCanEdit) ButtonEdit.Visibility = IsCanEdit ? Visibility.Visible : Visibility.Collapsed;
-            this.DataContext = this.Device;
+            if (IsCanEdit)
+            {
+                UserControl userControl = DeviceCalibration.GetEditControl();
+                if (userControl.Parent is Panel grid)
+                    grid.Children.Remove(userControl);
+                MQTTEditContent.Children.Add(userControl);
+            }
         }
         public void Dispose()
         {
