18a5209bed5116aa40067b0e11f49c0de10397f7
xincheng <xincheng213618@gmail.com>
2024-03-08 12:12:14 +0800

    Fix  update

--- ColorVision.Util/Draw/ToolShowImage.cs
+++ ColorVision.Util/Draw/ToolShowImage.cs
@@ -5,6 +5,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using System.Windows.Input;
 using ColorVision.Common.Extension;
+using Microsoft.VisualBasic.Devices;
 
 namespace ColorVision.Draw
 {
@@ -15,12 +16,16 @@ namespace ColorVision.Draw
 
         public DrawingVisual DrawVisualImage { get; set; }
 
+        public DrawingVisual DrawingVisualImage1 { get; set; }
+
+
 
         public ToolShowImage(ZoomboxSub zombox, DrawCanvas drawCanvas)
         {
             ZoomboxSub = zombox;
             Image = drawCanvas;
             DrawVisualImage = new DrawingVisual();
+            DrawingVisualImage1 = new DrawingVisual();
         }
 
         public bool IsShow
@@ -29,7 +34,6 @@ namespace ColorVision.Draw
             {
                 if (_IsShow == value) return;
                 _IsShow = value;
-
                 DrawVisualImageControl(_IsShow);
                 if (value)
                 {
@@ -65,55 +69,79 @@ namespace ColorVision.Draw
         {
             if (Image.Source is BitmapSource bitmapImage && disPoint.X > 60 && disPoint.X < bitmapImage.PixelWidth - 60 && disPoint.Y > 45 && disPoint.Y < bitmapImage.PixelHeight - 45)
             {
+
                 CroppedBitmap croppedBitmap = new CroppedBitmap(bitmapImage, new Int32Rect(disPoint.X.ToInt32() - 60, disPoint.Y.ToInt32() - 45, 120, 90));
 
                 using DrawingContext dc = DrawVisualImage.RenderOpen();
 
-                var transform = new MatrixTransform(1 / ZoomboxSub.ContentMatrix.M11, ZoomboxSub.ContentMatrix.M12, ZoomboxSub.ContentMatrix.M21, 1 / ZoomboxSub.ContentMatrix.M22, (1 - 1 / ZoomboxSub.ContentMatrix.M11) * actPoint.X, (1 - 1 / ZoomboxSub.ContentMatrix.M22) * actPoint.Y);
-                dc.PushTransform(transform);
+                double mouseX = actPoint.X; // 示例坐标
+                double mouseY = actPoint.Y; // 示例坐标
 
-                dc.DrawImage(croppedBitmap, new Rect(new Point(actPoint.X, actPoint.Y + 25), new Size(120, 90)));
+                double radius = 5; // 直径为10，半径为5
+                // 绘制空心圆
+                Pen circlePen = new Pen(Brushes.Black, 1); // 黑色笔刷，线宽为1
+                dc.DrawEllipse(null, circlePen, actPoint, radius, radius);
 
-                dc.DrawLine(new Pen(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00B1FF")), 3), new Point(actPoint.X + 59, actPoint.Y + 25), new Point(actPoint.X + 59, actPoint.Y + 25 + 90));
-                dc.DrawLine(new Pen(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00B1FF")), 3), new Point(actPoint.X, actPoint.Y + 25 + 44), new Point(actPoint.X + 120, actPoint.Y + 25 + 44));
+                // 绘制虚线的笔刷
+                Pen dashedPen = new Pen(Brushes.Black, 1)
+                {
+                    DashStyle = new DashStyle(new double[] { 2, 2 }, 0) // 虚线样式
+                };
+
+                // 绘制X轴虚线
+                dc.DrawLine(dashedPen, new Point(0, mouseY), new Point(mouseX - radius, mouseY)); // 左边
+                dc.DrawLine(dashedPen, new Point(mouseX + radius, mouseY), new Point(bitmapImage.PixelWidth, mouseY)); // 假设图像宽度为500
+
+                // 绘制Y轴虚线
+                dc.DrawLine(dashedPen, new Point(mouseX, 0), new Point(mouseX, mouseY - radius)); // 上边
+                dc.DrawLine(dashedPen, new Point(mouseX, mouseY + radius), new Point(mouseX, bitmapImage.PixelHeight)); // 假设图像高度为500
 
 
-                double x1 = actPoint.X;
-                double y1 = actPoint.Y + 25;
+                //var transform = new MatrixTransform(1 / ZoomboxSub.ContentMatrix.M11, ZoomboxSub.ContentMatrix.M12, ZoomboxSub.ContentMatrix.M21, 1 / ZoomboxSub.ContentMatrix.M22, (1 - 1 / ZoomboxSub.ContentMatrix.M11) * actPoint.X, (1 - 1 / ZoomboxSub.ContentMatrix.M22) * actPoint.Y);
+                //dc.PushTransform(transform);
 
-                double width = 120;
-                double height = 90;
+                //dc.DrawImage(croppedBitmap, new Rect(new Point(actPoint.X, actPoint.Y + 25), new Size(120, 90)));
+
+                //dc.DrawLine(new Pen(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00B1FF")), 3), new Point(actPoint.X + 59, actPoint.Y + 25), new Point(actPoint.X + 59, actPoint.Y + 25 + 90));
+                //dc.DrawLine(new Pen(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#00B1FF")), 3), new Point(actPoint.X, actPoint.Y + 25 + 44), new Point(actPoint.X + 120, actPoint.Y + 25 + 44));
 
 
-                dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1, y1 - 0.25), new Point(x1, y1 + height + 0.25));
-                dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1, y1), new Point(x1 + width, y1));
-                dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1 + width, y1 - 0.25), new Point(x1 + width, y1 + height + 0.25));
-                dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1, y1 + height), new Point(x1 + width, y1 + height));
+                //double x1 = actPoint.X;
+                //double y1 = actPoint.Y + 25;
 
-                x1++;
-                y1++;
-                width -= 2;
-                height -= 2;
-                dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1 - 0.75), new Point(x1, y1 + height + 0.75));
-                dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1), new Point(x1 + width, y1));
-                dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1 + width, y1 - 0.75), new Point(x1 + width, y1 + height + 0.75));
-                dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1 + height), new Point(x1 + width, y1 + height));
+                //double width = 120;
+                //double height = 90;
 
-                dc.DrawRectangle(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#AA000000")), new Pen(Brushes.White, 0), new Rect(x1 - 1, y1 + height + 1, width + 2, 45));
 
-                Brush brush = Brushes.White;
-                FontFamily fontFamily = new FontFamily("Arial");
-                double fontSize = 10;
-                FormattedText formattedText = new FormattedText($"R:{imageInfo.R}  G:{imageInfo.G}  B:{imageInfo.B}", System.Globalization.CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface(fontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal), fontSize, brush, VisualTreeHelper.GetDpi(DrawVisualImage).PixelsPerDip);
-                dc.DrawText(formattedText, new Point(x1 + 5, y1 + height + 5));
-                FormattedText formattedTex1 = new FormattedText($"({imageInfo.X},{imageInfo.Y})", System.Globalization.CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface(fontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal), fontSize, brush, VisualTreeHelper.GetDpi(DrawVisualImage).PixelsPerDip);
-                dc.DrawText(formattedTex1, new Point(x1 + 5, y1 + height + 31));
+                //dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1, y1 - 0.25), new Point(x1, y1 + height + 0.25));
+                //dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1, y1), new Point(x1 + width, y1));
+                //dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1 + width, y1 - 0.25), new Point(x1 + width, y1 + height + 0.25));
+                //dc.DrawLine(new Pen(Brushes.Black, 0.5), new Point(x1, y1 + height), new Point(x1 + width, y1 + height));
 
-                FormattedText formattedTex3 = new FormattedText($"{imageInfo.Hex}", System.Globalization.CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface(fontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal), fontSize, brush, VisualTreeHelper.GetDpi(DrawVisualImage).PixelsPerDip);
-                dc.DrawText(formattedTex3, new Point(x1 + 5, y1 + height + 18));
-                dc.Pop();
-                if (DrawVisualImage.Effect is not DropShadowEffect)
-                    DrawVisualImage.Effect = new DropShadowEffect() { Opacity = 0.5 };
+                //x1++;
+                //y1++;
+                //width -= 2;
+                //height -= 2;
+                //dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1 - 0.75), new Point(x1, y1 + height + 0.75));
+                //dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1), new Point(x1 + width, y1));
+                //dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1 + width, y1 - 0.75), new Point(x1 + width, y1 + height + 0.75));
+                //dc.DrawLine(new Pen(Brushes.White, 1.5), new Point(x1, y1 + height), new Point(x1 + width, y1 + height));
+
+                //dc.DrawRectangle(new SolidColorBrush((Color)ColorConverter.ConvertFromString("#AA000000")), new Pen(Brushes.White, 0), new Rect(x1 - 1, y1 + height + 1, width + 2, 45));
+
+                //Brush brush = Brushes.White;
+                //FontFamily fontFamily = new FontFamily("Arial");
+                //double fontSize = 10;
+                //FormattedText formattedText = new FormattedText($"R:{imageInfo.R}  G:{imageInfo.G}  B:{imageInfo.B}", System.Globalization.CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface(fontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal), fontSize, brush, VisualTreeHelper.GetDpi(DrawVisualImage).PixelsPerDip);
+                //dc.DrawText(formattedText, new Point(x1 + 5, y1 + height + 5));
+                //FormattedText formattedTex1 = new FormattedText($"({imageInfo.X},{imageInfo.Y})", System.Globalization.CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface(fontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal), fontSize, brush, VisualTreeHelper.GetDpi(DrawVisualImage).PixelsPerDip);
+                //dc.DrawText(formattedTex1, new Point(x1 + 5, y1 + height + 31));
+
+                //FormattedText formattedTex3 = new FormattedText($"{imageInfo.Hex}", System.Globalization.CultureInfo.CurrentCulture, FlowDirection.LeftToRight, new Typeface(fontFamily, FontStyles.Normal, FontWeights.Normal, FontStretches.Normal), fontSize, brush, VisualTreeHelper.GetDpi(DrawVisualImage).PixelsPerDip);
+                //dc.DrawText(formattedTex3, new Point(x1 + 5, y1 + height + 18));
+                //dc.Pop();
+                //if (DrawVisualImage.Effect is not DropShadowEffect)
+                //    DrawVisualImage.Effect = new DropShadowEffect() { Opacity = 0.5 };
 
             }
         }
