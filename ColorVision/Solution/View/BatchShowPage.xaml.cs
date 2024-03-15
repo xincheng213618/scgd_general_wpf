3fd8acad4a8ad08222d58e9a5ebfa45610e86be0
xincheng <xincheng213618@gmail.com>
2024-03-15 18:03:13 +0800

    update

---
+++ ColorVision/Solution/View/BatchShowPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorVision.Solution.View
+{
+    /// <summary>
+    /// BatchShowPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class BatchShowPage : Page
+    {
+        public Frame Frame { get; set; }
+        public ViewBatchResult ViewBatchResult { get; set; }
+
+        public BatchShowPage(Frame frame, ViewBatchResult viewBatchResult)
+        {
+            Frame = frame;
+            ViewBatchResult = viewBatchResult;
+            InitializeComponent();
+        }
+
+        private void Page_Initialized(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
