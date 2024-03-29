b273a38b51238ff9f40aa11cea105e1b3c1942b0
chenhong <chenhong@color-vision.com.cn>
2024-03-29 12:11:23 +0800

    算法CCT/Wave

--- ColorVision/Services/Devices/Algorithm/ConfigAlgorithm.cs
+++ ColorVision/Services/Devices/Algorithm/ConfigAlgorithm.cs
@@ -5,6 +5,8 @@ namespace ColorVision.Services.Devices.Algorithm
 {
     public class ConfigAlgorithm : DeviceServiceConfig, IServiceConfig
     {
+        public bool IsCCTWave { get => _IsCCTWave; set { _IsCCTWave = value; NotifyPropertyChanged(); } }
+        private bool _IsCCTWave;
         public string BindDeviceCode { get => _BindDeviceCode; set { _BindDeviceCode = value; NotifyPropertyChanged(); } }
         private string _BindDeviceCode;
         public FileServerCfg FileServerCfg { get; set; } = new FileServerCfg();
