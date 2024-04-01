2c022d2c4a04fcaa98384628853f0dfccd82c58e
xincheng <xincheng213618@gmail.com>
2024-04-01 14:13:03 +0800

    update

--- ColorVisionIcons64/resource2.h
+++ ColorVisionIcons64/resource2.h
@@ -1,15 +1,17 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ 生成的包含文件。
-// 供 GridIcons64.rc 使用
+// 供 ColorVisionIcons64.rc 使用
 //
-#define IDI_ICON1                       101
-#define IDI_ICON2                       102
+#define IDI_ICON1                       105
+#define IDI_ICON2                       106
+#define IDI_ICON3                       107
+#define IDI_ICON4                       108
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
