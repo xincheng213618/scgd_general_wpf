48a6af19a0cf86faaa9ea27e5785c804252315cc
xincheng <xincheng213618@gmail.com>
2024-04-01 14:28:37 +0800

    update

--- ColorVisionIcons64/resource2.h
+++ ColorVisionIcons64/resource2.h
@@ -4,8 +4,6 @@
 //
 #define IDI_ICON1                       105
 #define IDI_ICON2                       106
-#define IDI_ICON3                       107
-#define IDI_ICON4                       108
 
 // Next default values for new objects
 // 
