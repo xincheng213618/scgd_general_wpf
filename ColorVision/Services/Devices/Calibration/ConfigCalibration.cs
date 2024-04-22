879714c887d69c3f50413b12780484c79735903e
xincheng <xincheng213618@gmail.com>
2024-04-22 15:13:49 +0800

    update

--- ColorVision/Services/Devices/Calibration/ConfigCalibration.cs
+++ ColorVision/Services/Devices/Calibration/ConfigCalibration.cs
@@ -12,9 +12,9 @@ namespace ColorVision.Services.Devices.Calibration
 
         public double ExpTimeB { get => _ExpTimeB; set { _ExpTimeB = value; NotifyPropertyChanged(); } }
         private double _ExpTimeB = 10;
-
         public string BindCameraID { get => _BindCameraID; set { _BindCameraID = value; NotifyPropertyChanged(); } }
         private string _BindCameraID;
+
         public FileServerCfg FileServerCfg { get; set; } = new FileServerCfg();
     }
 }
