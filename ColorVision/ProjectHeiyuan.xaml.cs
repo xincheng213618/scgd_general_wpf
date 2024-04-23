ce42df19d6b8256375baf060a1f379b4680124a6
xincheng <xincheng213618@gmail.com>
2024-04-23 17:53:51 +0800

    update

--- ColorVision/ProjectHeiyuan.xaml.cs
+++ ColorVision/ProjectHeiyuan.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ColorVision.Common.MVVM;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +13,33 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static NPOI.HSSF.Util.HSSFColor;
 
 namespace ColorVision
 {
+    public class NumSet :ViewModelBase
+    {
+        public double White { get => _White; set { _White = value; NotifyPropertyChanged(); } }
+        private double _White;
+
+        public double Blue { get => _Blue; set { _Blue = value; NotifyPropertyChanged(); } }
+        private double _Blue;
+
+        public double Red { get => _Red; set { _Red = value; NotifyPropertyChanged(); } }
+        private double _Red;
+        public double Orange { get => _Orange; set { _Orange = value; NotifyPropertyChanged(); } }
+        private double _Orange;
+    }
+
+    public class TempResult : ViewModelBase
+    {
+        public string Name { get => _Name; set { _Name = value; NotifyPropertyChanged(); } }
+        private string _Name;
+
+        public NumSet NumSet { get; set; } = new NumSet();
+    }
+
+
     /// <summary>
     /// ProjectHeiyuan.xaml 的交互逻辑
     /// </summary>
@@ -24,12 +50,33 @@ namespace ColorVision
             InitializeComponent();
         }
 
-        bool result;
+        public ObservableCollection<TempResult> Settings { get; set; } = new ObservableCollection<TempResult>();
+        public ObservableCollection<TempResult> Results { get; set; } = new ObservableCollection<TempResult>();
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            Settings.Add(new TempResult() { Name = "x(上限)"});
+            Settings.Add(new TempResult() { Name = "x(下限)" });
+            Settings.Add(new TempResult() { Name = "y(上限)" });
+            Settings.Add(new TempResult() { Name = "y(下限)" });
+            Settings.Add(new TempResult() { Name = "lv(上限)" });
+            Settings.Add(new TempResult() { Name = "lv(下限)" });
+            ListViewSetting.ItemsSource = Settings;
+            Results.Add(new TempResult() { Name = "x" });
+            Results.Add(new TempResult() { Name = "y" });
+            Results.Add(new TempResult() { Name = "z" });
+            ListViewResult.ItemsSource = Results;
+        }
+
+
+        bool result =true;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             result = !result;
             ResultText.Text = result ? "OK" : "不合格";
-            ResultText.Foreground = result ? Brushes.Green : Brushes.Red;
+            ResultText.Foreground = result ? Brushes.Blue : Brushes.Red;
         }
+
+
     }
 }
