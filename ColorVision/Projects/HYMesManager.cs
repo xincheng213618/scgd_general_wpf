8f16a0b38abaac4b0b902f5eaf37f9af5288cf02
xincheng <xincheng213618@gmail.com>
2024-04-25 10:07:09 +0800

    新增类和方法，优化UI布局
    在`ProjectHeyuan.xaml.cs`文件中，添加了新的命名空间引用和新的类`ProjectHeyuan`，并在该类中定义了两个`ObservableCollection<TempResult>`类型的公共属性`Settings`和`Results`。同时，定义了一个名为`Button_Click`的方法，该方法在按钮点击时切换`result`的值，并根据`result`的值改变`ResultText`的文本和前景色。
    
    在`SerialMsg.cs`和`TempResult.cs`文件中，分别创建了新的类`SerialMsg`和`TempResult`，并在这些类中定义了一些公共属性。
    
    此外，还在`ProjectHeyuan.xaml`文件中添加了新的代码，定义了一个窗口的布局和元素。

---
+++ ColorVision/Projects/HYMesManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.ObjectModel;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ColorVision.Projects
+{
+    public class HYMesManager
+    {
+        private static HYMesManager _instance;
+        private static readonly object _locker = new();
+        public static HYMesManager GetInstance() 
+        {
+            
+            lock (_locker) {
+                if (_instance ==null)
+                    _instance = new HYMesManager();
+                return _instance;
+            } 
+        }
+
+        public ObservableCollection<SerialMsg> SerialMsgs { get; set; } = new ObservableCollection<SerialMsg>();
+
+        private SerialPort serialPort { get; set; }
+
+        public HYMesManager()
+        {
+            serialPort = new SerialPort { };
+        }
+
+        public int OpenPort(string portName)
+        {
+            try
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort = new SerialPort { PortName = portName, BaudRate = 115200 };
+                    serialPort.Open();
+                    string SetMsg = $"/CSN,C,0,TEST202303030002";
+
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(SetMsg);
+                    serialPort.Write(buffer, 0, buffer.Length);
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Thread.Sleep(16);
+                        int bytesread = serialPort.BytesToRead;
+                        if (bytesread > 0)
+                        {
+                            byte[] buff = new byte[bytesread];
+                            serialPort.Read(buff, 0, bytesread);
+                            if (buff.Length == 8 && buff[3] == 64)
+                            {
+                                serialPort.DataReceived += SerialPort_DataReceived;
+                                return 0;
+                            }
+                        }
+                    }
+                    serialPort.Close();
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch
+            {
+                return -2;
+            }
+
+
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+        }
+
+
+        public void SendSn(string ch,string sn)
+        {
+            string SendMsg = $"CSN,C,{ch},{sn}";
+            Send(System.Text.Encoding.UTF8.GetBytes(SendMsg));
+        }
+
+        public void Send(byte[] msg)
+        {
+            byte[] framedMsg = new byte[msg.Length + 2];
+            framedMsg[0] = 0x02; // STX (Start of Text)
+            msg.CopyTo(framedMsg, 1); // Copy original message into the new array starting at index 1
+            framedMsg[framedMsg.Length - 1] = 0x03; // ETX (End of Text)
+
+              SerialMsgs.Add(new SerialMsg() { Bytes = framedMsg });
+            if (serialPort.IsOpen)
+                serialPort.Write(msg, 0, msg.Length);
+        }
+
+
+        public int Initialized()
+        {
+            string[] TempPortNames = SerialPort.GetPortNames();
+            //这种写法不允许有多个串口；
+            for (int i = 0; i < TempPortNames.Length; i++)
+            {
+                if (OpenPort(TempPortNames[i]) == 0)
+                {
+                    return 0;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ ColorVision/Projects/ProjectHeyuan.xaml.cs
@@ -0,0 +1,72 @@
+﻿using ColorVision.Common.Sorts;
+using ColorVision.Update;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static NPOI.HSSF.Util.HSSFColor;
+
+namespace ColorVision.Projects
+{
+
+
+    /// <summary>
+    /// ProjectHeyuan.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProjectHeyuan : Window
+    {
+        public ProjectHeyuan()
+        {
+            InitializeComponent();
+        }
+
+        public ObservableCollection<TempResult> Settings { get; set; } = new ObservableCollection<TempResult>();
+        public ObservableCollection<TempResult> Results { get; set; } = new ObservableCollection<TempResult>();
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            Settings.Add(new TempResult() { Name = "x(上限)"});
+            Settings.Add(new TempResult() { Name = "x(下限)" });
+            Settings.Add(new TempResult() { Name = "y(上限)" });
+            Settings.Add(new TempResult() { Name = "y(下限)" });
+            Settings.Add(new TempResult() { Name = "lv(上限)" });
+            Settings.Add(new TempResult() { Name = "lv(下限)" });
+            ListViewSetting.ItemsSource = Settings;
+            Results.Add(new TempResult() { Name = "x" });
+            Results.Add(new TempResult() { Name = "y" });
+            Results.Add(new TempResult() { Name = "z" });
+            ListViewResult.ItemsSource = Results;
+
+            ComboBoxSer.ItemsSource = SerialPort.GetPortNames();
+            ComboBoxSer.SelectedIndex = 0;
+
+            ListViewMes.ItemsSource = HYMesManager.GetInstance().SerialMsgs;
+        }
+
+
+        bool result =true;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            result = !result;
+            ResultText.Text = result ? "OK" : "不合格";
+            ResultText.Foreground = result ? Brushes.Blue : Brushes.Red;
+
+
+            HYMesManager.GetInstance().SendSn("0","2222");
+
+        }
+
+
+    }
+}
