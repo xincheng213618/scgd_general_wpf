06a3ca0915e43fffa6ade16e59a2666874d3577e
xincheng <xincheng213618@gmail.com>
2024-04-28 02:21:59 +0800

    update

--- ColorVision/Services/Dao/Validate/ValidateParam.cs
+++ ColorVision/Services/Dao/Validate/ValidateParam.cs
@@ -1,12 +1,28 @@
-﻿using ColorVision.Services.Templates;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ColorVision.Common.MVVM;
+using ColorVision.Common.Utilities;
+using ColorVision.Services.Templates;
+using ColorVision.UI;
+using System.Windows;
 
 namespace ColorVision.Services.Dao.Validate
 {
+
+    public class BuildPOIMenuItem : IMenuItem
+    {
+        public string? OwnerGuid => "Template";
+
+        public string? GuidId => "Validue";
+        public int Index => 2;
+        public string? Header => "校验模板设置(_B)";
+
+        public string? InputGestureText { get; }
+
+        public string? Icon { get; }
+
+        public RelayCommand Command => new RelayCommand(a => {
+            new WindowTemplate(TemplateType.BuildPOIParmam) { Owner = Application.Current.GetActiveWindow(), WindowStartupLocation = WindowStartupLocation.CenterOwner }.ShowDialog(); ;
+        });
+    }
 
     public class ValidateParam : ParamBase
     {
