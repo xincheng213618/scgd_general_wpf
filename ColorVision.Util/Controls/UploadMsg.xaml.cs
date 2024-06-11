fb5c68fab825b989b78a0ce8d94d727c3868777c
xincheng <xincheng213618@gmail.com>
2024-06-11 15:27:01 +0800

    update

--- ColorVision.Util/Controls/UploadMsg.xaml.cs
+++ ColorVision.Util/Controls/UploadMsg.xaml.cs
@@ -1,5 +1,4 @@
-﻿using ColorVision.Common.MVVM;
-using System;
+﻿using System;
 using System.Drawing;
 using System.Globalization;
 using System.Windows;
@@ -8,66 +7,22 @@ using System.Windows.Documents;
 
 namespace ColorVision.Util.Controls
 {
-    public sealed class ColorConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is bool isconnect)
-            {
-                return isconnect ? Brushes.Blue : Brushes.Red;
-            }
-            return Brushes.Black; ;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotSupportedException("Converting from a string to a memory size is not supported.");
-        }
-    }
-
-    public class FileUploadInfo:ViewModelBase
-    {
-        public RelayCommand OpenFilePathCommand { get; set; }
-
-        public FileUploadInfo()
-        {
-
-        }
-
-        public string FileName { get => _FileName; set { _FileName = value; NotifyPropertyChanged(); } }
-        private string _FileName;
-
-        public string FilePath { get => _FilePath; set { _FilePath = value; NotifyPropertyChanged(); } }
-        private string _FilePath;
-
-        public string FileSize { get => _FileSize; set { _FileSize = value; NotifyPropertyChanged(); } }
-        private string _FileSize;
-        public string FileProgressValue { get => _FileProgressValue; set { _FileProgressValue = value; NotifyPropertyChanged(); } }
-        private string _FileProgressValue;
-        public UploadStatus UploadStatus { get => _UploadStatus; set { _UploadStatus = value; NotifyPropertyChanged(); } }
-        private UploadStatus _UploadStatus;
-
-    }
-
-
-
-
     /// <summary>
     /// UploadMsg.xaml 的交互逻辑
     /// </summary>
     public partial class UploadMsg : Window
     {
-        public IUploadMsg IUploadMsg { get; set; }
+        public IUploadMsg IUploadMsg1 { get; set; }
         public UploadMsg(IUploadMsg iUploadMsg)
         {
-            IUploadMsg = iUploadMsg;
+            IUploadMsg1 = iUploadMsg;
             InitializeComponent();
         }
 
         private void Window_Initialized(object sender, EventArgs e)
         {
-            DataContext = IUploadMsg;
-            IUploadMsg.UploadClosed += (s, e) => Close();
+            DataContext = IUploadMsg1;
+            IUploadMsg1.UploadClosed += (s, e) => Close();
         }
     }
 }
