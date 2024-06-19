c8d23ad9d86808f91d62a6a56e031facfdb7f3b8
xincheng <xincheng213618@gmail.com>
2024-06-20 03:23:50 +0800

    update

--- ColorVisionSetup/Utils.cs
+++ ColorVisionSetup/Utils.cs
@@ -14,20 +14,6 @@ namespace ColorVisionSetup
 {
     public class Utils
     {
-        public const string InstallationFolderName = "\\LGHUB";
-
-        private const string _settingsFileName = "\\settings.db";
-
-        private const string _frontendExecutableName = "\\lghub.exe";
-
-        private const string _uninstallerExecutableName = "\\lghub_software_manager.exe";
-
-        private static bool _logToFile;
-
-        private static string _logFilePath;
-
-        private static string _securedTempDirPath;
-
         private static Semaphore semaphore;
 
         private static bool unique;
@@ -37,65 +23,6 @@ namespace ColorVisionSetup
 
         }
 
-        public static void EnableLogs()
-        {
-
-        }
-
-        public static string GetProgramFilesPath()
-        {
-            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "LGHUB");
-        }
-
-        public static string GetProgramDataPath()
-        {
-            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "LGHUB");
-        }
-
-        public static string GetLocalAppDataPath()
-        {
-            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LGHUB");
-        }
-
-
-        public static string GetSecuredTempDirPath()
-        {
-            return _securedTempDirPath;
-        }
-
-        public static void SetSecuredTempDirPath(string folder_path)
-        {
-            _securedTempDirPath = folder_path;
-        }
-
-        public static bool CreateRandomSecuredTempDirectory()
-        {
-            _securedTempDirPath = "";
-            for (int i = 0; i < 5; i++)
-            {
-                try
-                {
-                    string path = "ghub-" + Path.GetRandomFileName();
-                    _securedTempDirPath = Path.Combine(Path.GetTempPath(), path);
-                    if (Directory.Exists(_securedTempDirPath))
-                    {
-                        continue;
-                    }
-                    DirectorySecurity directorySecurity = new DirectorySecurity();
-                    directorySecurity.AddAccessRule(GetAdminAccessRule());
-                    directorySecurity.AddAccessRule(GetSystemAccessRule());
-                    directorySecurity.AddAccessRule(GetWorldAccessRule());
-                    Directory.CreateDirectory(_securedTempDirPath, directorySecurity);
-                    Log("Utils: created random temp directory : " + _securedTempDirPath);
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    Log("Utils::createRandomSecuredTempDirectory failed -> " + ex.Message);
-                }
-            }
-            return false;
-        }
 
         private static FileSystemAccessRule GetAdminAccessRule()
         {
@@ -128,26 +55,6 @@ namespace ColorVisionSetup
             }
         }
 
-        public static bool IsAlreadyInstalled()
-        {
-            return Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\{521c89be-637f-4274-a840-baaf7460c2b2}", "DisplayName", null) != null;
-        }
-
-        public static bool IsLGSInstalled()
-        {
-            object value = Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Logitech Gaming Software", "VersionMajor", null);
-            object value2 = Registry.GetValue("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Logitech Gaming Software", "VersionMinor", null);
-            if (value == null || value2 == null)
-            {
-                return false;
-            }
-            if ((int)value > 8 || ((int)value == 8 && (int)value2 >= 98))
-            {
-                return false;
-            }
-            return true;
-        }
-
         public static void FocusExistingApp()
         {
             semaphore = new Semaphore(0, 1, Assembly.GetExecutingAssembly().GetName().Name, out unique);
@@ -169,45 +76,6 @@ namespace ColorVisionSetup
             Environment.Exit(0);
         }
 
-        public static bool SettingsExist()
-        {
-            if (Directory.Exists(GetLocalAppDataPath()) && File.Exists(GetLocalAppDataPath() + "\\settings.db"))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public static bool NextJsonExist()
-        {
-            if (Directory.Exists(GetProgramDataPath()) && File.Exists(GetProgramDataPath() + "/next.json"))
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public static bool StartGHUB()
-        {
-            if (Directory.Exists(GetProgramFilesPath()))
-            {
-                string text = GetProgramFilesPath() + "\\lghub.exe";
-                if (File.Exists(text))
-                {
-                    try
-                    {
-                        Process.Start(text);
-                        return true;
-                    }
-                    catch (Exception ex)
-                    {
-                        Log("Utils::StartGHUB -> " + ex.Message);
-                        return false;
-                    }
-                }
-            }
-            return false;
-        }
 
         public static void OpenExternalHyperlink(object sender, RequestNavigateEventArgs e)
         {
