9f2a63a89ce6f4488be717a97800549e005acef5
xincheng <xincheng213618@gmail.com>
2024-06-21 15:51:37 +0800

    优化曝光参数配置和界面调整
    对EditCamera.xaml、ConfigPhyCamera.cs、CreateWindow.xaml和EditPhyCamera.xaml进行了一系列更改，以优化自动曝光参数配置和界面的用户体验。在EditCamera.xaml中，更新了自动曝光参数的描述，并调整了电机移动区间上限的绑定。ConfigPhyCamera.cs中新增了IsChannelThree属性，用于判断通道是否为ImageChannel.Three，同时移除了与曝光时间和饱和度相关的所有属性和字段，以及ExpTimeCfg属性。CreateWindow.xaml中更新了滤色轮数量的绑定。EditPhyCamera.xaml中移除了默认参数配置的GroupBox，修改了滤色轮GroupBox的Visibility绑定，使其始终可见，并移除了自动曝光参数配置的GroupBox。这些更改旨在简化配置过程，提高软件的易用性和灵活性。

--- ColorVision.Engine/Services/PhyCameras/Configs/ConfigPhyCamera.cs
+++ ColorVision.Engine/Services/PhyCameras/Configs/ConfigPhyCamera.cs
@@ -174,6 +174,7 @@ namespace ColorVision.Engine.Services.PhyCameras.Configs
             get => TakeImageMode != TakeImageMode.Live && (CameraType == CameraType.CV_Q || CameraType == CameraType.CV_MIL_CL);
             set => NotifyPropertyChanged();
         }
+
         [JsonIgnore]
         public bool IsChannelThree
         {
@@ -181,40 +182,9 @@ namespace ColorVision.Engine.Services.PhyCameras.Configs
             set => NotifyPropertyChanged();
         }
 
-        public int ExpTime { get => _ExpTime; set { _ExpTime = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(ExpTimeLog)); } }
-        private int _ExpTime = 10;
-
-        public double ExpTimeLog { get => Math.Log(ExpTime); set { ExpTime = (int)Math.Pow(Math.E, value); } }
-
-        public int ExpTimeR { get => _ExpTimeR; set { _ExpTimeR = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(ExpTimeRLog)); } }
-        private int _ExpTimeR = 10;
-
-        public double ExpTimeRLog { get => Math.Log(ExpTimeR); set { ExpTimeR = (int)Math.Pow(Math.E, value); } }
-
-        public int ExpTimeG { get => _ExpTimeG; set { _ExpTimeG = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(ExpTimeGLog)); } }
-        private int _ExpTimeG = 10;
-        public double ExpTimeGLog { get => Math.Log(ExpTimeG); set { ExpTimeG = (int)Math.Pow(Math.E, value); } }
-
-        public int ExpTimeB { get => _ExpTimeB; set { _ExpTimeB = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(ExpTimeBLog)); } }
-        private int _ExpTimeB = 10;
-        public double ExpTimeBLog { get => Math.Log(ExpTimeB); set { ExpTimeB = (int)Math.Pow(Math.E, value); } }
-
-
-        public double Saturation { get => _Saturation; set { _Saturation = value; NotifyPropertyChanged(); } }
-        private double _Saturation = -1;
-
-        public double SaturationR { get => _SaturationR; set { _SaturationR = value; NotifyPropertyChanged(); } }
-        private double _SaturationR = -1;
-
-        public double SaturationG { get => _SaturationG; set { _SaturationG = value; NotifyPropertyChanged(); } }
-        private double _SaturationG = -1;
-
-        public double SaturationB { get => _SaturationB; set { _SaturationB = value; NotifyPropertyChanged(); } }
-        private double _SaturationB = -1;
         public MotorConfig MotorConfig { get; set; } = new MotorConfig();
         public PhyCameraCfg CameraCfg { get; set; } = new PhyCameraCfg();
         public CFWPORT CFW { get; set; } = new CFWPORT();
-        public PhyExpTimeCfg ExpTimeCfg { get; set; } = new PhyExpTimeCfg();
         public FileSeviceConfig FileServerCfg { get; set; } = new FileSeviceConfig();
     }
 
