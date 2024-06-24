c733173ba3779b74255c15d62772b5c0e1498669
xincheng <xincheng213618@gmail.com>
2024-06-25 02:49:53 +0800

    update

--- ColorVision.Engine/Services/SysDictionary/Dao/SysDictionaryModDao.cs
+++ ColorVision.Engine/Services/SysDictionary/Dao/SysDictionaryModDao.cs
@@ -8,21 +8,26 @@ namespace ColorVision.Engine.Services.SysDictionary
 {
     public class SysDictionaryModModel : ViewModelBase,IPKModel
     {
+        [Column("id")]
         public int Id { get; set; }
+        [Column("pid")]
         public int? PId { get => _PId; set { _PId = value; NotifyPropertyChanged(); } }
         private int? _PId;
-
+        [Column("mod_type")]
         public short ModType { get => _ModType; set { _ModType = value; NotifyPropertyChanged(); } }
         private short _ModType;
 
+        [Column("code")]
         public string? Code { get => _Code; set { _Code = value; NotifyPropertyChanged(); } }
         private string? _Code;
-
+        [Column("name")]
         public string? Name { get => _Name; set { _Name = value; NotifyPropertyChanged(); } }
         private string? _Name;
 
+        [Column("tenant_id")]
         public int TenantId { get => _TenantId; set { _TenantId = value; NotifyPropertyChanged(); } }
         private int _TenantId;
+        [Column("create_date")]
         public DateTime CreateDate { get => _CreateDate; set { _CreateDate = value; NotifyPropertyChanged(); } }
         private DateTime _CreateDate;
     }
@@ -34,59 +39,7 @@ namespace ColorVision.Engine.Services.SysDictionary
         public SysDictionaryModDao() : base("t_scgd_sys_dictionary_mod_master", "id")
         {
         }
-        public override DataTable CreateColumns(DataTable dataTable)
-        {
-            dataTable.Columns.Add("id", typeof(int));
-            dataTable.Columns.Add("code", typeof(string));
-            dataTable.Columns.Add("name", typeof(string));
-            dataTable.Columns.Add("tenant_id", typeof(int));
-            dataTable.Columns.Add("pid", typeof(int?));
-            dataTable.Columns.Add("mod_type", typeof(short));
-            dataTable.Columns.Add("create_date", typeof(DateTime));
-            return dataTable;        
-        }
 
-        public override DataRow Model2Row(SysDictionaryModModel item, DataRow row)
-        {
-            if (item.Id > 0) row["id"] = item.Id;
-            row["code"] = DataTableExtension.IsDBNull(item.Code);
-            row["name"] = DataTableExtension.IsDBNull(item.Name);
-            row["tenant_id"] = DataTableExtension.IsDBNull(item.TenantId);
-            row["pid"] = DataTableExtension.IsDBNull(item.PId);
-            row["mod_type"] = DataTableExtension.IsDBNull(item.ModType);
-            row["create_date"] = DataTableExtension.IsDBNull(item.CreateDate);
-            return row;
-            
-        }
-
-        public override SysDictionaryModModel GetModelFromDataRow(DataRow item)
-        {
-            SysDictionaryModModel model = new()
-            {
-                Id = item.Field<int>("id"),
-                Code = item.Field<string>("code"),
-                Name = item.Field<string>("name"),
-                TenantId = item.Field<int>("tenant_id"),
-                PId = item.Field<int?>("pid"),
-                ModType = item.Field<short>("mod_type"),
-                CreateDate = item.Field<DateTime>("create_date")
-            };
-
-            return model;
-        }
-
-        public SysDictionaryModModel GetByCode(string? code, int tenantId)
-        {
-            if (string.IsNullOrEmpty(code))
-                return new SysDictionaryModModel();
-            string sql = $"select * from {TableName} where is_delete=0 and code=@code and tenant_id=@tenantId";
-            Dictionary<string, object> param = new()
-            {
-                { "code", code },
-                { "tenantId", tenantId }
-            };
-            DataTable d_info = GetData(sql, param);
-            return d_info.Rows.Count == 1 ? GetModelFromDataRow(d_info.Rows[0]) : new SysDictionaryModModel();
-        }
+        public SysDictionaryModModel? GetByCode(string? code, int tenantId) => GetByParam( new Dictionary<string, object>() { { "is_delete",0 }, { "code", code }, { "tenant_id", tenantId } });
     }
 }
