171ff958c63222dedb3e65b153d3c6d8f8a6214b
xincheng <xincheng213618@gmail.com>
2024-07-01 15:45:11 +0800

    Fix BUG

--- ColorVision.Engine/Services/Devices/Camera/Video/CameraVideoConfig.cs
+++ ColorVision.Engine/Services/Devices/Camera/Video/CameraVideoConfig.cs
@@ -1,4 +1,6 @@
 ﻿using ColorVision.Common.MVVM;
+using System.Collections.Generic;
+using System;
 
 namespace ColorVision.Engine.Services.Devices.Camera.Video
 {
@@ -29,7 +31,103 @@ namespace ColorVision.Engine.Services.Devices.Camera.Video
         }
         private int _Port = 9002;
 
-        public long Capacity { get => _Capacity; set { _Capacity = value; NotifyPropertyChanged(); } }
+        public long Capacity
+        {
+            get => _Capacity;
+            set
+            {
+                if (_Capacity != value)
+                {
+                    _Capacity = value;
+                    NotifyPropertyChanged();
+                    NotifyPropertyChanged(nameof(CapacityText));
+                    NotifyPropertyChanged(nameof(CapacityInput));
+                }
+            }
+        }
         private long _Capacity = 1073741824;
+
+        public string CapacityText => MemorySizeText(_Capacity);
+
+        public string CapacityInput
+        {
+            get => MemorySizeText(_Capacity);
+            set
+            {
+                if (TryParseMemorySize(value, out long parsedValue))
+                {
+                    Capacity = parsedValue;
+                }
+            }
+        }
+
+        public static string MemorySizeText(long memorySize)
+        {
+            const long KB = 1024;
+            const long MB = KB * 1024;
+            const long GB = MB * 1024;
+            const long TB = GB * 1024;
+            const long PB = TB * 1024;
+
+            var units = new[]
+            {
+            Tuple.Create(PB, "PB"),
+            Tuple.Create(TB, "TB"),
+            Tuple.Create(GB, "GB"),
+            Tuple.Create(MB, "MB"),
+            Tuple.Create(KB, "kB"),
+            Tuple.Create(1L, "Byte")
+        };
+
+            foreach (var unit in units)
+            {
+                if (memorySize >= unit.Item1)
+                {
+                    double value = (double)memorySize / unit.Item1;
+                    if (memorySize < unit.Item1 * 10)
+                    {
+                        return $"{value:F1} {unit.Item2}";
+                    }
+                    return $"{(long)value} {unit.Item2}";
+                }
+            }
+            return "0 Byte";
+        }
+
+        public static bool TryParseMemorySize(string input, out long memorySize)
+        {
+            input = input.Trim().ToUpperInvariant();
+            memorySize = 0;
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            var units = new Dictionary<string, long>
+        {
+            { "PB", 1024L * 1024 * 1024 * 1024 * 1024 },
+            { "TB", 1024L * 1024 * 1024 * 1024 },
+            { "GB", 1024L * 1024 * 1024 },
+            { "MB", 1024L * 1024 },
+            { "KB", 1024L },
+            { "B", 1L }
+        };
+
+            foreach (var unit in units)
+            {
+                if (input.EndsWith(unit.Key))
+                {
+                    if (double.TryParse(input.Substring(0, input.Length - unit.Key.Length), out double value))
+                    {
+                        memorySize = (long)(value * unit.Value);
+                        return true;
+                    }
+                }
+            }
+
+            return long.TryParse(input, out memorySize); // Try to parse as bytes if no unit is found
+        }
+
     }
 }
