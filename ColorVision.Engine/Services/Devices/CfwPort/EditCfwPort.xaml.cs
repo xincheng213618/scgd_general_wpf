3c0a1ccb6f7889082afe10668f69e5bc9bcf745f
xincheng <xincheng213618@gmail.com>
2024-07-03 11:36:34 +0800

    UPDATE

--- ColorVision.Engine/Services/Devices/CfwPort/EditCfwPort.xaml.cs
+++ ColorVision.Engine/Services/Devices/CfwPort/EditCfwPort.xaml.cs
@@ -2,6 +2,7 @@
 using ColorVision.Engine.Services.PhyCameras;
 using ColorVision.Themes;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 
@@ -38,6 +39,11 @@ namespace ColorVision.Engine.Services.Devices.CfwPort
             DataContext = Device;
             EditConfig = Device.Config.Clone();
             EditContent.DataContext = EditConfig;
+
+            List<int> BaudRates = new() { 115200, 38400, 9600, 300, 600, 1200, 2400, 4800, 14400, 19200, 57600 };
+            List<string> Serials = new() { "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "COM10" };
+            ComboBoxPort.ItemsSource = Serials;
+            ComboBoxBaudRates.ItemsSource = BaudRates;
 
             CameraPhyID.ItemsSource = PhyCameraManager.GetInstance().PhyCameras;
             CameraPhyID.DisplayMemberPath = "Code";
