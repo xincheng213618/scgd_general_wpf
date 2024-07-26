a4b39ec33f40d2bb54d532dce0a0e70b95d2fd4f
xincheng <xincheng213618@gmail.com>
2024-07-26 14:51:10 +0800

    update

--- ColorVision.Engine/Media/ImageView.xaml.cs
+++ ColorVision.Engine/Media/ImageView.xaml.cs
@@ -15,15 +15,12 @@ using CVCommCore.CVAlgorithm;
 using CVCommCore.CVImage;
 using log4net;
 using MQTTMessageLib.FileServer;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,37 +31,6 @@ using System.Windows.Media.Imaging;
 
 namespace ColorVision.Engine.Media
 {
-    public class ImageViewConfig:ViewModelBase
-    {
-        [JsonIgnore]
-        public string FilePath { get => _FilePath; set { _FilePath = value; NotifyPropertyChanged(); } }
-        private string _FilePath;
-
-        [JsonIgnore]
-        public IntPtr ConvertXYZhandle { get; set; } = Tool.GenerateRandomIntPtr();
-
-        [JsonIgnore]
-        public bool ConvertXYZhandleOnce { get; set; }
-
-        [JsonIgnore]
-        public bool IsCVCIE { get => _IsCVCIE; set { _IsCVCIE = value; NotifyPropertyChanged(); }  }
-        private bool _IsCVCIE;
-
-        [JsonIgnore]
-        public int Channel { get => _Channel; set { _Channel = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(IsChannel1)); NotifyPropertyChanged(nameof(IsChannel3)); } }
-        private int _Channel;
-
-        [JsonIgnore]
-        public bool IsChannel1 => Channel == 1;
-        [JsonIgnore]
-        public bool IsChannel3 => Channel == 3;
-
-        public bool IsShowLoadImage { get => _IsShowLoadImage; set { _IsShowLoadImage = value; NotifyPropertyChanged(); } }
-        private bool _IsShowLoadImage = true;
-
-        public ColormapTypes ColormapTypes { get => _ColormapTypes; set { _ColormapTypes = value; NotifyPropertyChanged(); } }
-        private ColormapTypes _ColormapTypes = ColormapTypes.COLORMAP_JET;
-    }
 
 
 
