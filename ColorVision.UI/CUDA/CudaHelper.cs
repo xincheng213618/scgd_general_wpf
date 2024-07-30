ee168cc582138e4ef772a4f23a5cb5f4c28b593a
xincheng <xincheng213618@gmail.com>
2024-07-30 23:58:01 +0800

    移除不必要的 using 语句
    删除了以下 using 语句：
    - `using System;`
    - `using System.Collections.Generic;`
    - `using System.Linq;`
    - `using System.Text;`
    - `using System.Threading.Tasks;`
    - `using System.Windows.Controls;`

--- ColorVision.UI/CUDA/CudaHelper.cs
+++ ColorVision.UI/CUDA/CudaHelper.cs
@@ -1,12 +1,6 @@
 ﻿using ColorVision.Common.MVVM;
 using ColorVision.UI.Configs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
 
 namespace ColorVision.UI.CUDA
 {
