6e3940f4a1eaa9a50e75c2aa1741f97130eabb10
xincheng <xincheng213618@gmail.com>
2024-08-05 02:22:46 +0800

    update

---
+++ ColorVision.UI/CUDA/SystemInitializer.cs
@@ -0,0 +1,36 @@
+﻿#pragma warning disable CA1806
+using System.Runtime.InteropServices;
+
+namespace ColorVision.UI.CUDA
+{
+    public class SystemInitializer : IInitializer
+    {
+        public int Order => 8;
+
+        private readonly IMessageUpdater _messageUpdater;
+
+        public SystemInitializer(IMessageUpdater messageUpdater)
+        {
+            _messageUpdater = messageUpdater;
+        }
+
+
+
+        public async Task InitializeAsync()
+        {
+            await Task.Delay(30);
+            _messageUpdater.UpdateMessage("Debug Mode: " + SystemHelper.IsDebugMode());
+            _messageUpdater.UpdateMessage("OS Version: " + SystemHelper.GetOSVersion());
+            _messageUpdater.UpdateMessage(".NET Version: " + SystemHelper.GetDotNetVersion());
+            _messageUpdater.UpdateMessage("Application Version: " + SystemHelper.GetApplicationVersion());
+            _messageUpdater.UpdateMessage("User Name: " + SystemHelper.GetUserName());
+            _messageUpdater.UpdateMessage("Machine Name: " + SystemHelper.GetMachineName());
+            _messageUpdater.UpdateMessage("Memory Info: " + SystemHelper.GetMemoryInfo());
+            _messageUpdater.UpdateMessage("System Language: " + SystemHelper.GetSystemLanguage());
+            _messageUpdater.UpdateMessage("Screen Resolution: " + SystemHelper.GetScreenResolution());
+            await Task.Delay(30);
+
+        }
+
+    }
+}
