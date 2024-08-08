9bd9f8adf8327e1e44246049fd98f797cf375501
xincheng <xincheng213618@gmail.com>
2024-08-08 11:33:26 +0800

    update

---
+++ Plugins/ScreenRecorder/ExportEventWindow.cs
@@ -0,0 +1,28 @@
+﻿using ColorVision.Common.MVVM;
+using ColorVision.Common.Utilities;
+using ColorVision.Themes;
+using ColorVision.UI.Authorizations;
+using ColorVision.UI.Menus;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Windows;
+
+namespace ScreenRecorder
+{
+    public class ExportEventWindow : MenuItemBase
+    {
+        public override string OwnerGuid => "Tool";
+        public override string GuidId => "ScreenRecorder";
+        public override int Order => 1000;
+        public override string Header => "录制视频";
+
+        [RequiresPermission(PermissionMode.Administrator)]
+        public override void Execute()
+        {
+            new MainWindow() { Owner = Application.Current.GetActiveWindow(), WindowStartupLocation = WindowStartupLocation.CenterOwner }.ShowDialog();
+        }
+    }
+
+}
