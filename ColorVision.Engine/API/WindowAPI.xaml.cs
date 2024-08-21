b0f316a2f0c7d02177dec2e250cfee347c992769
xincheng <xincheng213618@gmail.com>
2024-08-21 11:36:56 +0800

    update

---
+++ ColorVision.Engine/API/WindowAPI.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Windows;
+
+namespace ColorVision.Engine.API
+{
+    /// <summary>
+    /// WindowAPI.xaml 的交互逻辑
+    /// </summary>
+    public partial class WindowAPI : Window
+    {
+        public WindowAPI()
+        {
+            InitializeComponent();
+        }
+
+        private void StartApiHost_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+    }
+}
