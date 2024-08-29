dc6deddfa8279730118711457042ef352cd889c9
xincheng <xincheng213618@gmail.com>
2024-08-30 01:32:05 +0800

    更改数据源类型并添加默认实例处理
    - 将 `CreateDicModeDetail.xaml.cs` 文件中的 `ComboBoxValueType.ItemsSource` 数据源从 `ValueType` 枚举类型更改为 `SValueType` 枚举类型。
    - 将 `SysDictionaryModDetailDao.cs` 文件中的 `ValueType` 属性及其私有字段 `_ValueType` 类型从 `ValueType` 更改为 `SValueType`，并将 `IsEnable` 属性的私有字段 `_IsEnable` 初始化为 `true`。
    - 删除 `ValueType.cs` 文件中的 `ValueType` 枚举定义。
    - 在 `CreateModeDetail.xaml.cs` 文件中添加了 `ScottPlot.Styles` 的引用。
    - 在 `CreateModeDetail.xaml.cs` 文件中，当 `SysDictionaryModDetaiModels` 为空时，添加一个默认的 `SysDictionaryModDetaiModel` 实例，并将其保存到数据库和列表中。
    - 在 `SValueType.cs` 文件中添加了新的 `SValueType` 枚举定义。

--- ColorVision.Engine/Templates/CreateModeDetail.xaml.cs
+++ ColorVision.Engine/Templates/CreateModeDetail.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ColorVision.Engine.MySql.ORM;
 using ColorVision.Engine.Services.SysDictionary;
 using ColorVision.Themes;
+using ScottPlot.Styles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,15 +38,20 @@ namespace ColorVision.Engine.Templates
             SysDictionaryModDetaiModels = SysDictionaryModDetailDao.Instance.GetAllByPid(Param.ModMaster.Pid);
             BorderEdit.DataContext = CreateConfig;
 
-            if (SysDictionaryModDetaiModels.Count > 0)
+            if (SysDictionaryModDetaiModels.Count == 0)
             {
-                var values = SysDictionaryModDetaiModels
+                int nid = SysDictionaryModDetailDao.Instance.GetNextAvailableId();
+                SysDictionaryModDetaiModel sysDictionaryModDetaiModel = new SysDictionaryModDetaiModel() { Id = nid, AddressCode = nid, PId = Param.Id, Symbol = "default", Name = "default", DefaultValue = "", ValueType = SValueType.String };
+                SysDictionaryModDetailDao.Instance.Save(sysDictionaryModDetaiModel);
+                SysDictionaryModDetaiModels.Add(sysDictionaryModDetaiModel);
+            }
+
+            var values = SysDictionaryModDetaiModels
                 .Select(item => new KeyValuePair<int, string>((int)item.AddressCode, item.Name ?? item.Symbol ?? item.AddressCode.ToString()))
                 .ToList();
-                CreateConfig.SysPid = (int)SysDictionaryModDetaiModels[0].AddressCode;
-                CreateConfig.ValueA = SysDictionaryModDetaiModels[0].DefaultValue;
-                ComboBoxSymbol.ItemsSource = values;
-            }
+            CreateConfig.SysPid = (int)SysDictionaryModDetaiModels[0].AddressCode;
+            CreateConfig.ValueA = SysDictionaryModDetaiModels[0].DefaultValue;
+            ComboBoxSymbol.ItemsSource = values;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
