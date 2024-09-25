c50bdbd4c56aef9b9e92858d39a61a87d8fdcb9b
xincheng <xincheng213618@gmail.com>
2024-09-25 11:50:18 +0800

    ledcheck2

---
+++ ColorVision.Engine/Services/Devices/Algorithm/Templates/LedCheck2/AlgorithmLedCheck2.cs
@@ -0,0 +1,79 @@
+﻿using ColorVision.Common.MVVM;
+using ColorVision.Common.Utilities;
+using ColorVision.Engine.Services.Devices.ThirdPartyAlgorithms.Templates;
+using ColorVision.Engine.Services.Msg;
+using ColorVision.Engine.Templates;
+using ColorVision.Engine.Templates.POI;
+using MQTTMessageLib;
+using MQTTMessageLib.Algorithm;
+using MQTTMessageLib.FileServer;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.LedCheck2
+{
+    public  class AlgorithmLedCheck2 : ViewModelBase, IAlgorithm
+    {
+        public string Name { get; set; } = "灯珠检测2";
+
+        public DeviceAlgorithm Device { get; set; }
+        public MQTTAlgorithm DService { get => Device.DService; }
+
+        public RelayCommand OpenTemplateCommand { get; set; }
+        public RelayCommand OpenTemplatePoiCommand { get; set; }
+
+        public AlgorithmLedCheck2(DeviceAlgorithm deviceAlgorithm)
+        {
+            Device = deviceAlgorithm;
+            OpenTemplateCommand = new RelayCommand(a => OpenTemplate());
+            OpenTemplatePoiCommand = new RelayCommand(a => OpenTemplatePoi());
+        }
+        public int TemplateSelectedIndex { get => _TemplateSelectedIndex; set { _TemplateSelectedIndex = value; NotifyPropertyChanged(); } }
+        private int _TemplateSelectedIndex;
+
+        public void OpenTemplate()
+        {
+            new WindowTemplate( new TemplateThirdParty("LedCheck2"), TemplateSelectedIndex) { Owner = Application.Current.GetActiveWindow(), WindowStartupLocation = WindowStartupLocation.CenterOwner }.ShowDialog();
+        }
+        public int TemplatePoiSelectedIndex { get => _TemplatePoiSelectedIndex; set { _TemplatePoiSelectedIndex = value; NotifyPropertyChanged(); } }
+        private int _TemplatePoiSelectedIndex;
+        public void OpenTemplatePoi()
+        {
+            new WindowTemplate(new TemplatePOI(), TemplatePoiSelectedIndex) { Owner = Application.Current.GetActiveWindow(), WindowStartupLocation = WindowStartupLocation.CenterOwner }.ShowDialog();
+        }
+
+
+
+
+        public UserControl GetUserControl()
+        {
+            UserControl ??= new DisplayLedCheck2(this);
+            return UserControl;
+        }
+        public UserControl UserControl { get; set; }
+
+
+        public MsgRecord SendCommand(ModThirdPartyParam param, PoiParam poiParam, CVOLEDCOLOR cOLOR,string deviceCode, string deviceType, string fileName, FileExtType fileExtType, string serialNumber)
+        {
+            string sn = null;
+            if (string.IsNullOrWhiteSpace(serialNumber)) sn = DateTime.Now.ToString("yyyyMMdd'T'HHmmss.fffffff");
+            else sn = serialNumber;
+
+            var Params = new Dictionary<string, object>() { { "ImgFileName", fileName }, { "FileType", fileExtType }, { "DeviceCode", deviceCode }, { "DeviceType", deviceType } };
+            Params.Add("TemplateParam", new CVTemplateParam() { ID = param.Id, Name = param.Name });
+            Params.Add("POITemplateParam", new CVTemplateParam() { ID = poiParam.Id, Name = poiParam.Name });
+            Params.Add("Color", cOLOR);
+
+            MsgSend msg = new()
+            {
+                EventName = MQTTAlgorithmEventEnum.Event_LED_Check_GetData,
+                SerialNumber = sn,
+                Params = Params
+            };
+
+            return DService.PublishAsyncClient(msg);
+        }
+    }
+}
---
+++ ColorVision.Engine/Services/Devices/Algorithm/Templates/LedCheck2/DisplayLedCheck2.xaml.cs
@@ -0,0 +1,135 @@
+﻿using ColorVision.Common.Utilities;
+using ColorVision.Engine.Services.Devices.ThirdPartyAlgorithms.Templates;
+using ColorVision.Engine.Services.Msg;
+using ColorVision.Engine.Templates;
+using ColorVision.Engine.Templates.POI;
+using ColorVision.Themes.Controls;
+using MQTTMessageLib.FileServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.LedCheck2
+{
+    /// <summary>
+    /// DisplaySFR.xaml 的交互逻辑
+    /// </summary>
+    public partial class DisplayLedCheck2 : UserControl
+    {
+        public AlgorithmLedCheck2 IAlgorithm { get; set; }
+        public DisplayLedCheck2(AlgorithmLedCheck2 iAlgorithm)
+        {
+            IAlgorithm = iAlgorithm;
+            InitializeComponent();
+        }
+
+        private void UserControl_Initialized(object sender, EventArgs e)
+        {
+            DataContext = IAlgorithm;
+            
+            ComboxTemplate.ItemsSource = TemplateThirdParty.Params.GetValue("LedCheck2");
+            ComboxTemplate.SelectedIndex = 0;
+
+            ComboxPoiTemplate.ItemsSource = TemplatePOI.Params.CreateEmpty();
+            ComboxPoiTemplate.SelectedIndex = 0;
+
+
+            ComboxCVOLEDCOLOR.ItemsSource = from e1 in Enum.GetValues(typeof(CVOLEDCOLOR)).Cast<CVOLEDCOLOR>()
+                                            select new KeyValuePair<string, CVOLEDCOLOR>(e1.ToString(), e1);
+            ComboxCVOLEDCOLOR.SelectedIndex = 0;
+
+            void UpdateCB_SourceImageFiles()
+            {
+                CB_SourceImageFiles.ItemsSource = ServiceManager.GetInstance().GetImageSourceServices();
+                CB_SourceImageFiles.SelectedIndex = 0;
+            }
+            ServiceManager.GetInstance().DeviceServices.CollectionChanged += (s, e) => UpdateCB_SourceImageFiles();
+            UpdateCB_SourceImageFiles();
+        }
+
+        private void RunTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            if (!AlgorithmHelper.IsTemplateSelected(ComboxTemplate, "请先选择灯珠检测模板")) return;
+            if (!AlgorithmHelper.IsTemplateSelected(ComboxPoiTemplate, "请先选择关注点模板")) return;
+
+            if (ComboxTemplate.SelectedValue is not ModThirdPartyParam param) return;
+            if (ComboxPoiTemplate.SelectedValue is not PoiParam poiParam) return;
+            if (ComboxCVOLEDCOLOR.SelectedValue is not CVOLEDCOLOR color) return;
+
+
+            if (GetAlgSN(out string sn, out string imgFileName, out FileExtType fileExtType))
+            {
+                string type = string.Empty;
+                string code = string.Empty;
+                if (CB_SourceImageFiles.SelectedItem is DeviceService deviceService)
+                {
+                    type = deviceService.ServiceTypes.ToString();
+                    code = deviceService.Code;
+                }
+                MsgRecord msg = IAlgorithm.SendCommand(param, poiParam, color,type, code, imgFileName, fileExtType, sn);
+                ServicesHelper.SendCommand(msg, "LEDStripDetection");
+            }
+        }
+
+        private bool GetAlgSN(out string sn, out string imgFileName, out FileExtType fileExtType)
+        {
+            sn = string.Empty;
+            fileExtType = FileExtType.Tif;
+            imgFileName = string.Empty;
+
+            bool? isSN = AlgBatchSelect.IsSelected;
+            bool? isRaw = AlgRawSelect.IsSelected;
+
+            if (isSN == true)
+            {
+                if (string.IsNullOrWhiteSpace(AlgBatchCode.Text))
+                {
+                    MessageBox1.Show(Application.Current.MainWindow, "批次号不能为空，请先输入批次号", "ColorVision");
+                    return false;
+                }
+                sn = AlgBatchCode.Text;
+            }
+            else if (isRaw == true)
+            {
+                imgFileName = CB_RawImageFiles.Text;
+                fileExtType = FileExtType.Raw;
+            }
+            else
+            {
+                imgFileName = ImageFile.Text;
+            }
+            if (string.IsNullOrWhiteSpace(imgFileName))
+            {
+                MessageBox1.Show(Application.Current.MainWindow, "图像文件不能为空，请先选择图像文件", "ColorVision");
+                return false;
+            }
+            return true;
+        }
+
+        private void Open_File(object sender, RoutedEventArgs e)
+        {
+            using var openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png,*.tif) | *.jpg; *.jpeg; *.png;*.tif";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ImageFile.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Button_Click_RawRefresh(object sender, RoutedEventArgs e)
+        {
+            if (CB_SourceImageFiles.SelectedItem is not DeviceService deviceService) return;
+            IAlgorithm.DService.GetRawFiles(deviceService.Code, deviceService.ServiceTypes.ToString());
+        }
+
+        private void Button_Click_Open(object sender, RoutedEventArgs e)
+        {
+            if (CB_SourceImageFiles.SelectedItem is DeviceService deviceService)
+                IAlgorithm.DService.Open(deviceService.Code, deviceService.ServiceTypes.ToString(), CB_RawImageFiles.Text, FileExtType.CIE);
+        }
+    }
+}
