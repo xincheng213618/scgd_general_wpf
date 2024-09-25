23f66e77e09ed63e8bad92d26e9b49e614b62fba
xincheng <xincheng213618@gmail.com>
2024-09-25 10:35:09 +0800

    update

--- ColorVision.Engine/Services/Devices/Algorithm/Views/AlgorithmView.xaml.cs
+++ ColorVision.Engine/Services/Devices/Algorithm/Views/AlgorithmView.xaml.cs
@@ -5,7 +5,7 @@ using ColorVision.Engine.MySql.ORM;
 using ColorVision.Engine.Services.Devices.Algorithm.Templates.BuildPoi;
 using ColorVision.Engine.Services.Devices.Algorithm.Templates.Compliance;
 using ColorVision.Engine.Services.Devices.Algorithm.Templates.Distortion;
-using ColorVision.Engine.Services.Devices.Algorithm.Templates.FOV;
+using ColorVision.Engine.Services.Devices.Algorithm.Templates.LEDStripDetection;
 using ColorVision.Engine.Services.Devices.Algorithm.Templates.Ghost;
 using ColorVision.Engine.Services.Devices.Algorithm.Templates.LedCheck;
 using ColorVision.Engine.Services.Devices.Algorithm.Templates.MTF;
