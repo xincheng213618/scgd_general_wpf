64b952bbfe49dcecef406513e581da5d3617f9d6
xincheng <xincheng213618@gmail.com>
2024-09-26 16:48:16 +0800

    重构模板和MySQL命令实现
    重构了多个文件以改进模板和MySQL命令的实现：
    - 在 `TemplateJND.cs` 中，添加了 `GetMysqlCommand` 方法，返回 `MysqlJND` 实例。
    - 在 `ExportROIParam.cs` 中，移除了整个 `ExportRoiParam` 类。
    - 在 `TemplateRoi.cs` 中，添加了 `GetMysqlCommand` 方法，返回 `MysqlRoi` 实例。
    - 在 `MysqlRoi.cs` 中，添加了新的 `MysqlJND` 和 `MysqlRoi` 类，均实现 `IMysqlCommand` 接口。
    - 在 `ExportRoi.cs` 中，添加了新的 `ExportRoi` 类，继承自 `ExportTemplateBase`。

---
+++ ColorVision.Engine/Services/Devices/Algorithm/Templates/ROI/MysqlRoi.cs
@@ -0,0 +1,16 @@
+﻿using ColorVision.Engine.MySql;
+
+namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.ROI
+{
+    public class MysqlRoi : IMysqlCommand
+    {
+        public string GetMysqlCommandName() => "恢复MysqlMysqlROI";
+
+        public string GetRecover()
+        {
+            string t_scgd_sys_dictionary_mod_master = "UPDATE `t_scgd_sys_dictionary_mod_master` SET `code` = 'OLED.GetROI', `name` = 'ROI', `pid` = NULL, `mod_type` = 7, `create_date` = '2024-08-16 15:27:12', `is_enable` = 1, `is_delete` = 0, `remark` = NULL, `tenant_id` = 0 WHERE `id` = 31;\r\n";
+            string t_scgd_sys_dictionary_mod_item = "INSERT INTO `t_scgd_sys_dictionary_mod_item` (`id`, `symbol`, `address_code`, `name`, `val_type`, `value_range`, `default_val`, `pid`, `create_date`, `is_enable`, `is_delete`, `remark`) VALUES (4310, 'Threshold', 4310, '阈值', 0, NULL, '1', 31, '2023-11-14 17:44:15', 1, 0, NULL);\r\nINSERT INTO `t_scgd_sys_dictionary_mod_item` (`id`, `symbol`, `address_code`, `name`, `val_type`, `value_range`, `default_val`, `pid`, `create_date`, `is_enable`, `is_delete`, `remark`) VALUES (4311, 'Times', 4311, 'Times', 0, NULL, '1', 31, '2023-11-14 17:44:15', 1, 0, NULL);\r\nINSERT INTO `t_scgd_sys_dictionary_mod_item` (`id`, `symbol`, `address_code`, `name`, `val_type`, `value_range`, `default_val`, `pid`, `create_date`, `is_enable`, `is_delete`, `remark`) VALUES (4312, 'SmoothSize', 4312, 'SmoothSize', 0, NULL, '1', 31, '2023-11-14 17:44:15', 1, 0, NULL);";
+            return t_scgd_sys_dictionary_mod_master + t_scgd_sys_dictionary_mod_item;
+        }
+    }
+}
--- ColorVision.Engine/Services/Devices/Algorithm/Templates/ROI/TemplateRoi.cs
+++ ColorVision.Engine/Services/Devices/Algorithm/Templates/ROI/TemplateRoi.cs
@@ -1,4 +1,5 @@
-﻿using ColorVision.Engine.Templates;
+﻿using ColorVision.Engine.MySql;
+using ColorVision.Engine.Templates;
 using System.Collections.ObjectModel;
 
 namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.ROI
@@ -12,6 +13,11 @@ namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.ROI
             Title = "发光区检测2";
             Code = "OLED.GetROI";
             TemplateParams = Params;
+        }
+
+        public override IMysqlCommand? GetMysqlCommand()
+        {
+            return new MysqlRoi();
         }
     }
 }
