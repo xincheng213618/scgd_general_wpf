fb8a9a54a935146bb811b3b982c44e2d7ce26cf4
xincheng <xincheng213618@gmail.com>
2024-09-30 13:06:13 +0800

    UPDATE

--- ColorVision.Engine/Services/Devices/Algorithm/Templates/JND/AlgorithmJND.cs
+++ ColorVision.Engine/Services/Devices/Algorithm/Templates/JND/AlgorithmJND.cs
@@ -2,6 +2,7 @@
 using ColorVision.Common.Utilities;
 using ColorVision.Engine.Services.Msg;
 using ColorVision.Engine.Templates;
+using ColorVision.Engine.Templates.POI;
 using MQTTMessageLib;
 using MQTTMessageLib.Algorithm;
 using MQTTMessageLib.FileServer;
@@ -20,11 +21,13 @@ namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.JND
         public MQTTAlgorithm DService { get => Device.DService; }
 
         public RelayCommand OpenTemplateCommand { get; set; }
+        public RelayCommand OpenTemplatePoiCommand { get; set; }
 
         public AlgorithmJND(DeviceAlgorithm deviceAlgorithm)
         {
             Device = deviceAlgorithm;
             OpenTemplateCommand = new RelayCommand(a => OpenTemplate());
+            OpenTemplatePoiCommand = new RelayCommand(a => OpenTemplatePoi());
         }
 
         public int TemplateSelectedIndex { get => _TemplateSelectedIndex; set { _TemplateSelectedIndex = value; NotifyPropertyChanged(); } }
@@ -34,6 +37,14 @@ namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.JND
         {
             new WindowTemplate(new TemplateJND(), TemplateSelectedIndex) { Owner = Application.Current.GetActiveWindow(), WindowStartupLocation = WindowStartupLocation.CenterOwner }.ShowDialog();
         }
+        public int TemplatePoiSelectedIndex { get => _TemplatePoiSelectedIndex; set { _TemplatePoiSelectedIndex = value; NotifyPropertyChanged(); } }
+        private int _TemplatePoiSelectedIndex;
+
+        public void OpenTemplatePoi()
+        {
+            new WindowTemplate(new TemplatePoi(), _TemplatePoiSelectedIndex) { Owner = Application.Current.GetActiveWindow(), WindowStartupLocation = WindowStartupLocation.CenterOwner }.ShowDialog(); ;
+        }
+
 
         public UserControl GetUserControl()
         {
@@ -51,6 +62,8 @@ namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.JND
 
             var Params = new Dictionary<string, object>() { { "ImgFileName", fileName }, { "FileType", fileExtType }, { "DeviceCode", deviceCode }, { "DeviceType", deviceType } };
             Params.Add("TemplateParam", new CVTemplateParam() { ID = param.Id, Name = param.Name });
+            
+            Params.Add("POITemplateParam", new CVTemplateParam() { ID = TemplatePoi.Params[TemplatePoiSelectedIndex].Value.Id, Name = TemplatePoi.Params[TemplatePoiSelectedIndex].Value.Name });
 
             MsgSend msg = new()
             {
