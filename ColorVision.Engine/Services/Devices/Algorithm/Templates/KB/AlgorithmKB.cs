1ec390dc6907f184d4ec5bf2b8f1f60a69ce294b
xincheng <xincheng213618@gmail.com>
2024-10-07 00:18:05 +0800

    update

---
+++ ColorVision.Engine/Services/Devices/Algorithm/Templates/KB/AlgorithmKB.cs
@@ -0,0 +1,37 @@
+﻿using ColorVision.Common.MVVM;
+using ColorVision.Common.Utilities;
+using ColorVision.Engine.Services.Msg;
+using ColorVision.Engine.Templates;
+using ColorVision.Engine.Templates.POI;
+using MQTTMessageLib;
+using MQTTMessageLib.Algorithm;
+using MQTTMessageLib.FileServer;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.KB
+{
+    public class AlgorithmKB : ViewModelBase, IDisplayAlgorithm
+    {
+        public string Name { get; set; } = "KB";
+
+        public DeviceAlgorithm Device { get; set; }
+        public MQTTAlgorithm DService { get => Device.DService; }
+
+
+        public AlgorithmKB(DeviceAlgorithm deviceAlgorithm)
+        {
+            Device = deviceAlgorithm;
+        }
+
+        public UserControl GetUserControl()
+        {
+            UserControl ??= new DisplayKB(this);
+            return UserControl;
+        }
+        public UserControl UserControl { get; set; }
+
+    }
+}
---
+++ ColorVision.Engine/Services/Devices/Algorithm/Templates/KB/DisplayKB.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ColorVision.Engine.Services.Msg;
+using ColorVision.Engine.Templates;
+using ColorVision.Engine.Templates.POI;
+using ColorVision.Themes.Controls;
+using MQTTMessageLib.FileServer;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using static OpenCvSharp.ML.SVM;
+
+namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.KB
+{
+    /// <summary>
+    /// DisplaySFR.xaml 的交互逻辑
+    /// </summary>
+    public partial class DisplayKB : UserControl
+    {
+        public AlgorithmKB IAlgorithm { get; set; }
+        public DisplayKB(AlgorithmKB iAlgorithm)
+        {
+            IAlgorithm = iAlgorithm;
+            InitializeComponent();
+        }
+
+        private void UserControl_Initialized(object sender, EventArgs e)
+        {
+            DataContext = IAlgorithm;
+
+        }
+
+        private void RunTemplate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
