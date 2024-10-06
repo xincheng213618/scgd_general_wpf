466820c61f7610ce2aa4c127f201976ee96f7037
xincheng <xincheng213618@gmail.com>
2024-10-07 03:51:57 +0800

    update

---
+++ Plugins/WindowsServicePlugin/InstallService.cs
@@ -0,0 +1,83 @@
+﻿using ColorVision.Common.Utilities;
+using ColorVision.Themes.Controls;
+using ColorVision.UI;
+using ColorVision.UI.Menus;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace WindowsServicePlugin
+{
+    public class InstallService : MenuItemBase, IWizardStep
+    {
+        public override string OwnerGuid => "ServiceLog";
+
+        public override string GuidId => "InstallService";
+
+        public override int Order => 99;
+        public override string Header => "InstallService";
+
+
+        public DownloadFile DownloadFile { get; set; } = new DownloadFile();
+        public InstallService()
+        {
+            DownloadFile = new DownloadFile();
+            DownloadFile.DownloadTile = "下载最新的Service";
+        }
+
+        private string url = "http://xc213618.ddns.me:9999/D%3A/ColorVision/Tool/CVWindowsService/FullPackage%5B1.7.0.929%5D-0929.zip";
+        private string downloadPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\" + @"ColorVision\\FullPackage[1.7.0.929]-0929.exe";
+        public override void Execute()
+        {
+            WindowUpdate windowUpdate = new WindowUpdate(DownloadFile);
+            if (!File.Exists(downloadPath))
+            {
+                windowUpdate.Show();
+            }
+
+            Task.Run(async () =>
+            {
+                if (!File.Exists(downloadPath))
+                {
+                    await DownloadFile.GetIsPassWorld();
+                    CancellationTokenSource _cancellationTokenSource = new();
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        windowUpdate.Show();
+                    });
+                    await DownloadFile.Download(url, downloadPath, _cancellationTokenSource.Token);
+                }
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    windowUpdate.Close();
+                });
+                // 启动新的实例
+                ProcessStartInfo startInfo = new();
+                startInfo.UseShellExecute = true; // 必须为true才能使用Verb属性
+                startInfo.WorkingDirectory = Environment.CurrentDirectory;
+                startInfo.FileName = downloadPath;
+                startInfo.Verb = "runas"; // "runas"指定启动程序时请求管理员权限
+                                          // 如果需要静默安装，添加静默安装参数
+                                          //quiet 没法自启，桌面图标也是空                       
+                                          //startInfo.Arguments = "/quiet";
+
+                try
+                {
+                    Process p = Process.Start(startInfo);
+                    p?.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    File.Delete(downloadPath);
+                }
+            });
+
+
+        }
+
+    }
+
+
+
+}
---
+++ Plugins/WindowsServicePlugin/InstallWinrar.cs
@@ -0,0 +1,84 @@
+﻿using ColorVision.Common.Utilities;
+using ColorVision.Themes.Controls;
+using ColorVision.UI;
+using ColorVision.UI.Menus;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace WindowsServicePlugin
+{
+    public class InstallWinrar : MenuItemBase, IWizardStep
+    {
+        public override string OwnerGuid => "ServiceLog";
+
+        public override string GuidId => "InstallWinrar";
+
+        public override int Order => 99;
+        public override string Header => "InstallWinrar";
+
+
+        public DownloadFile DownloadFile { get; set; } = new DownloadFile();
+        public InstallWinrar()
+        {
+            DownloadFile = new DownloadFile();
+            DownloadFile.DownloadTile = "InstallWinrar";
+        }
+
+        private string url = "http://xc213618.ddns.me:9999/D%3A/ColorVision/Tool/Winrar/winrar-x64-700sc.exe";
+        private string downloadPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\" + @"ColorVision\\winrar-x64-700sc.exe";
+
+        public override void Execute()
+        {
+            WindowUpdate windowUpdate = new WindowUpdate(DownloadFile);
+            if (!File.Exists(downloadPath))
+            {
+                windowUpdate.Show();
+            }
+
+            Task.Run(async () =>
+            {
+                if (!File.Exists(downloadPath))
+                {
+                    await DownloadFile.GetIsPassWorld();
+                    CancellationTokenSource _cancellationTokenSource = new();
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        windowUpdate.Show();
+                    });
+                    await DownloadFile.Download(url, downloadPath, _cancellationTokenSource.Token);
+                }
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    windowUpdate.Close();
+                });
+                // 启动新的实例
+                ProcessStartInfo startInfo = new();
+                startInfo.UseShellExecute = true; // 必须为true才能使用Verb属性
+                startInfo.WorkingDirectory = Environment.CurrentDirectory;
+                startInfo.FileName = downloadPath;
+                startInfo.Verb = "runas"; // "runas"指定启动程序时请求管理员权限
+                                          // 如果需要静默安装，添加静默安装参数
+                                          //quiet 没法自启，桌面图标也是空                       
+                                          //startInfo.Arguments = "/quiet";
+
+                try
+                {
+                    Process p = Process.Start(startInfo);
+                    p?.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                    File.Delete(downloadPath);
+                }
+            });
+
+
+        }
+
+    }
+
+
+
+}
