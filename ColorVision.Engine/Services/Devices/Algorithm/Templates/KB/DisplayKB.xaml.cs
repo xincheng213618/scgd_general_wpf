4690f1b1bb161c6804af9917b7449c57f92ed251
xincheng <xincheng213618@gmail.com>
2024-10-09 16:03:19 +0800

    update

--- ColorVision.Engine/Services/Devices/Algorithm/Templates/KB/DisplayKB.xaml.cs
+++ ColorVision.Engine/Services/Devices/Algorithm/Templates/KB/DisplayKB.xaml.cs
@@ -2,6 +2,7 @@
 using ColorVision.Engine.Templates;
 using ColorVision.Engine.Templates.POI;
 using ColorVision.Themes.Controls;
+using cvColorVision;
 using MQTTMessageLib.FileServer;
 using System;
 using System.Windows;
@@ -30,7 +31,20 @@ namespace ColorVision.Engine.Services.Devices.Algorithm.Templates.KB
 
         private void RunTemplate_Click(object sender, RoutedEventArgs e)
         {
+            IRECT rect = new IRECT(0, 0, 100, 50);
+            int width = 1920, height = 1080, bpp = 24, channels = 3;
+            float exposure = 1.0f;
+            string luminFile = "path/to/luminFile";
 
+            IntPtr imgData = IntPtr.Zero; // Assume this is initialized with image data
+            KeyBoardDLL.CM_InitialKeyBoardSrc(width, height, bpp, channels, imgData, 0, exposure, luminFile);
+            float haloGray = KeyBoardDLL.CM_CalculateHalo(rect, 20, 128, 18, "path/to/save");
+
+            float keyGray = KeyBoardDLL.CM_CalculateKey(rect, 30, 128, "path/to/save");
+            IntPtr pData = IntPtr.Zero; // Assume this is initialized with sufficient memory
+            int result = KeyBoardDLL.CM_GetKeyBoardResult(ref width, ref height, ref bpp, ref channels, pData);
+
+            Console.WriteLine($"Halo Gray: {haloGray}, Key Gray: {keyGray}, Result: {result}");
         }
     }
 }
---
+++ cvColorVision/KeyBoard.cs
@@ -0,0 +1,29 @@
+﻿#pragma warning disable CA2101,CA1401,CA1707,SYSLIB
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvColorVision
+{
+    public class KeyBoardDLL
+    {
+        private const string LIBRARY_CVCAMERA = "libs\\cvCamera.dll";
+
+
+        [DllImport(LIBRARY_CVCAMERA, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void CM_InitialKeyBoardSrc(int w, int h, int bpp, int channels, IntPtr imgdata, int saveProcessData, float exp, string luminFile);
+
+        [DllImport(LIBRARY_CVCAMERA, CallingConvention = CallingConvention.Cdecl)]
+        public static extern float CM_CalculateHalo(IRECT keyRect, int outMOVE, int threadV, int haloSize, string savePath);
+
+        [DllImport(LIBRARY_CVCAMERA, CallingConvention = CallingConvention.Cdecl)]
+        public static extern float CM_CalculateKey(IRECT keyRect, int inMOVE, int threadV, string path);
+
+        [DllImport(LIBRARY_CVCAMERA, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int CM_GetKeyBoardResult(ref int w, ref int h, ref int bpp, ref int channels, IntPtr pData);
+    }
+
+}
