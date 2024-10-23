b6ab2b61147871573ef86c8ba56a6ba3f72cbcd0
xincheng <xincheng213618@gmail.com>
2024-10-23 12:00:34 +0800

    update

---
+++ ColorVision.Engine/Templates/POI/POIFix/TemplatePoiFix.cs
@@ -0,0 +1,18 @@
+﻿using ColorVision.Engine.Services.Templates.POI.POIFix;
+using ColorVision.Engine.Templates;
+using System.Collections.ObjectModel;
+
+namespace ColorVision.Engine.Services.Templates.POI.POIFix
+{
+    public class TemplatePoiFix : ITemplate<PoiFixParam>, IITemplateLoad
+    {
+        public static ObservableCollection<TemplateModel<PoiFixParam>> Params { get; set; } = new ObservableCollection<TemplateModel<PoiFixParam>>();
+
+        public TemplatePoiFix()
+        {
+            Title = "PoiFix";
+            Code = "PoiFix";
+            TemplateParams = Params;
+        }
+    }
+}
