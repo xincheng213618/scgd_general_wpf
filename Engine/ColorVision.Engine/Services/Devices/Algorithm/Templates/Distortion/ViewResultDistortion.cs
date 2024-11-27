16ecb14d8f2c0989adc2a0e1cad81a992275237c
chenhong <chenhong@color-vision.com.cn>
2024-11-27 10:15:15 +0800

    update

--- Engine/ColorVision.Engine/Services/Devices/Algorithm/Templates/Distortion/ViewResultDistortion.cs
+++ Engine/ColorVision.Engine/Services/Devices/Algorithm/Templates/Distortion/ViewResultDistortion.cs
@@ -1,5 +1,6 @@
 ﻿#pragma  warning disable CA1708,CS8602,CS8604,CS8629
 using ColorVision.Common.MVVM;
+using CVCommCore.CVAlgorithm;
 using MQTTMessageLib.Algorithm;
 using Newtonsoft.Json;
 using System;
