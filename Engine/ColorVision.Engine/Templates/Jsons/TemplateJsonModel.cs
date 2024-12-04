d08f49ed7a861701c8b689c6fbb0bd5469b4c669
xincheng <xincheng213618@gmail.com>
2024-12-04 13:06:53 +0800

    update

--- Engine/ColorVision.Engine/Templates/Jsons/TemplateJsonModel.cs
+++ Engine/ColorVision.Engine/Templates/Jsons/TemplateJsonModel.cs
@@ -26,7 +26,6 @@ namespace ColorVision.Engine.Templates.Jsons
         public string? JsonVal { get => _JsonVal; set { _JsonVal = value; NotifyPropertyChanged(); } }
         private string? _JsonVal;
 
-
         [Column("is_enable")]
         public bool IsEnable { get => _IsEnable; set { _IsEnable = value; NotifyPropertyChanged(); } }
         private bool _IsEnable =true;
