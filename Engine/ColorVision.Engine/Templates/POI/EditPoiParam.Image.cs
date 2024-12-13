821afc63b22815e4d158ecb76271fc8dc031bac3
xincheng <xincheng213618@gmail.com>
2024-12-13 17:41:07 +0800

    update

--- Engine/ColorVision.Engine/Templates/POI/EditPoiParam.Image.cs
+++ Engine/ColorVision.Engine/Templates/POI/EditPoiParam.Image.cs
@@ -46,6 +46,27 @@ namespace ColorVision.Engine.Templates.POI
         }
     }
 
+    public class RoundToNearestHalfConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return Math.Round(doubleValue * 2, MidpointRounding.AwayFromZero) / 2;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return Math.Round(doubleValue * 2, MidpointRounding.AwayFromZero) / 2;
+            }
+            return value;
+        }
+    }
+
 
     public partial class EditPoiParam : Window
     {
