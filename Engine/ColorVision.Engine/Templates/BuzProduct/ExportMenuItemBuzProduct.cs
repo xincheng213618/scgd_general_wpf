51a04360c94bc0861c275044fdc5470394bd108d
xincheng <xincheng213618@gmail.com>
2024-12-24 15:21:38 +0800

    update

--- Engine/ColorVision.Engine/Templates/BuzProduct/ExportMenuItemBuzProduct.cs
+++ Engine/ColorVision.Engine/Templates/BuzProduct/ExportMenuItemBuzProduct.cs
@@ -4,7 +4,7 @@
     {
         public override string OwnerGuid => "Template";
         public override string GuidId => nameof(TemplateBuzProduc);
-        public override string Header => "TemplateBuzProduc";
+        public override string Header => "应用属性模板";
         public override int Order => 2;
         public override ITemplate Template => new TemplateBuzProduc();
     }
