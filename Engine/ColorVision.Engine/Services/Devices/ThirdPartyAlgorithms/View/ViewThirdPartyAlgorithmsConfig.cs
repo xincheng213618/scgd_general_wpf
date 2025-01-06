3a7c6cdd5a105b6cec54b29394a81fd5ebfc2efa
xincheng <xincheng213618@gmail.com>
2025-01-06 11:40:22 +0800

    update

--- Engine/ColorVision.Engine/Services/Devices/ThirdPartyAlgorithms/View/ViewThirdPartyAlgorithmsConfig.cs
+++ Engine/ColorVision.Engine/Services/Devices/ThirdPartyAlgorithms/View/ViewThirdPartyAlgorithmsConfig.cs
@@ -8,6 +8,7 @@ using ColorVision.UI;
 using ColorVision.UI.Sorts;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 
 namespace ColorVision.Engine.Services.Devices.ThirdPartyAlgorithms.Views
@@ -15,5 +16,27 @@ namespace ColorVision.Engine.Services.Devices.ThirdPartyAlgorithms.Views
     public class ViewThirdPartyAlgorithmsConfig : ViewConfigBase,IConfig
     {
         public static  ViewThirdPartyAlgorithmsConfig Instance => ConfigService.Instance.GetRequiredService<ViewThirdPartyAlgorithmsConfig>();
+
+        public ObservableCollection<GridViewColumnVisibility> GridViewColumnVisibilitys { get; set; } = new ObservableCollection<GridViewColumnVisibility>();
+
+        public ImageViewConfig ImageViewConfig { get; set; } = new ImageViewConfig();
+
+        [DisplayName("显示数据列"), Category("Control")]
+        public bool IsShowListView { get => _IsShowListView; set { _IsShowListView = value; NotifyPropertyChanged(); } }
+        private bool _IsShowListView = true;
+
+        [DisplayName("显示侧边栏"), Category("Control")]
+        public bool IsShowSideListView { get => _IsShowSideListView; set { _IsShowSideListView = value; NotifyPropertyChanged(); } }
+        private bool _IsShowSideListView = true;
+
+
+        [DisplayName("数据列保存路径")]
+        public string SaveSideDataDirPath { get => _SaveSideDataDirPath; set { _SaveSideDataDirPath = value; NotifyPropertyChanged(); } }
+        private string _SaveSideDataDirPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        [DisplayName("自动保存数据列")]
+        public bool AutoSaveSideData { get => _AutoSaveSideData; set { _AutoSaveSideData = value; NotifyPropertyChanged(); } }
+        private bool _AutoSaveSideData;
+
     }
 }
