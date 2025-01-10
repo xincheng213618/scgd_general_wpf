41e2ba7b78e8657241f3482103550984af691ce2
xincheng <xincheng213618@gmail.com>
2025-01-10 15:49:19 +0800

    update

--- Engine/ColorVision.Engine/DataHistory/Dao/ConfigArchivedDao.cs
+++ Engine/ColorVision.Engine/DataHistory/Dao/ConfigArchivedDao.cs
@@ -3,6 +3,7 @@ using ColorVision.Engine.MySql.ORM;
 using ColorVision.UI.Menus;
 using ColorVision.UI.PropertyEditor;
 using System.ComponentModel;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace ColorVision.Engine.DataHistory.Dao
@@ -36,7 +37,7 @@ namespace ColorVision.Engine.DataHistory.Dao
         public int Id { get => _Id; set { _Id = value; NotifyPropertyChanged(); } }
         private int _Id;
         [Column("path"), DisplayName("数据目录"), PropertyEditorType(PropertyEditorType.TextSelectFolder)]
-        public string Path { get => _Path.Replace("\\", "\\\\"); set { _Path = value; NotifyPropertyChanged(); } }
+        public string Path { get => Regex.Replace(_Path, @"(?<!\\)\\(?!\\)", @"\\"); set { _Path = value; NotifyPropertyChanged(); } }
         private string _Path;
         [Column("cron_expression"), DisplayName("Cron表达式"), PropertyEditorType(PropertyEditorType.CronExpression)]
         public string CronExpression { get => _CronExpression; set { _CronExpression = value; NotifyPropertyChanged(); } }
