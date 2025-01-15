7fecc7a8bed73ff196c928223d3feb35e56b9585
xincheng <xincheng213618@gmail.com>
2025-01-15 14:37:57 +0800

    update

--- Engine/ColorVision.Engine/Templates/Jsons/LedCheck2/DisplayLedCheck2.xaml.cs
+++ Engine/ColorVision.Engine/Templates/Jsons/LedCheck2/DisplayLedCheck2.xaml.cs
@@ -132,7 +132,7 @@ namespace ColorVision.Engine.Templates.Jsons.LedCheck2
         private void Open_File(object sender, RoutedEventArgs e)
         {
             using var openFileDialog = new System.Windows.Forms.OpenFileDialog();
-openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png, *.tif)|*.jpg;*.jpeg;*.png;*.tif|All files (*.*)|*.*";
+            openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png, *.tif)|*.jpg;*.jpeg;*.png;*.tif;*.cvcie;*.cvraw|All files (*.*)|*.*";
             openFileDialog.RestoreDirectory = true;
             openFileDialog.FilterIndex = 1;
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
