8391f578da4a2b44cfccdd79e94e5c47bb792cd9
xincheng <xincheng213618@gmail.com>
2025-03-04 15:37:50 +0800

    update

---
+++ Engine/ColorVision.Engine/Templates/ImageCropping/ResultImageDao.cs
@@ -0,0 +1,32 @@
+﻿using ColorVision.Engine.MySql.ORM;
+using ColorVision.Engine.Services.Devices.Algorithm;
+using System.Data;
+
+namespace ColorVision.Engine.Templates.ImageCropping
+{
+    [Table("t_scgd_algorithm_result_detail_image")]
+    public class ResultImageModel : VPKModel,IViewResult
+    {
+        [Column("pid")]
+        public int? Pid { get; set; }
+
+        [Column("file_name")]
+        public string? FileName { get; set; }
+
+        [Column("order_index")]
+        public int? OrderIndex { get; set; }
+
+        [Column("file_info")]
+        public string? FileInfo { get; set; }
+
+    }
+
+    public class ResultImageDao : BaseTableDao<ResultImageModel>
+    {
+        public static ResultImageDao Instance { get; set; } = new ResultImageDao();
+
+        public ResultImageDao() : base("t_scgd_algorithm_result_detail_image", "id")
+        {
+        }
+    }
+}
---
+++ Engine/ColorVision.Engine/Templates/ImageCropping/ViewHandleImageCropping.cs
@@ -0,0 +1,90 @@
+﻿using ColorVision.Engine.MySql.ORM;
+using ColorVision.Engine.Services.Devices.Algorithm;
+using ColorVision.Engine.Services.Devices.Algorithm.Views;
+using ColorVision.Engine.Templates.POI.AlgorithmImp;
+using ColorVision.ImageEditor.Draw;
+using CVCommCore.CVAlgorithm;
+using MQTTMessageLib.Algorithm;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ColorVision.Engine.Templates.ImageCropping
+{
+    public class ViewHandleImageCropping : IResultHandleBase
+    {
+        public override List<AlgorithmResultType> CanHandle { get; } = new List<AlgorithmResultType>() { AlgorithmResultType.Image_Cropping };
+
+        public override void SideSave(AlgorithmResult result, string selectedPath)
+        {
+            var ViewResults = result.ViewResults.ToSpecificViewResults<ResultImageModel>();
+
+            var csvBuilder = new StringBuilder();
+            List<string> properties = new() { "Id", "file_name", "order_index" , "FileInfo" };
+            // 写入列头
+            csvBuilder.AppendLine(string.Join(",", properties));
+            // 写入数据行
+            foreach (var item in ViewResults)
+            {
+                List<string> values = new()
+                {
+                    item.Id.ToString(),
+                    item.FileName ?? string.Empty,
+                    item.OrderIndex.ToString() ?? string.Empty,
+                    item.FileInfo ?? string.Empty
+                };
+
+                csvBuilder.AppendLine(string.Join(",", values));
+            }
+
+            File.WriteAllText(selectedPath, csvBuilder.ToString(), Encoding.UTF8);
+
+            string saveng = System.IO.Path.Combine(selectedPath, $"{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.png");
+            AlgorithmView.ImageView.ImageViewModel.Save(saveng);
+        }
+
+        public AlgorithmView AlgorithmView { get; set; }
+
+        public override void Handle(AlgorithmView view, AlgorithmResult result)
+        {
+            AlgorithmView = view;
+            view.ImageView.ImageShow.Clear();
+            if (result.ResultCode != 0)
+            {
+                if (File.Exists(result.FilePath))
+                    view.ImageView.OpenImage(result.FilePath);
+                return;
+            }
+
+            if (File.Exists(result.FilePath))
+                view.ImageView.OpenImage(result.FilePath);
+
+            if (result.ViewResults == null)
+            {
+                result.ViewResults = new ObservableCollection<IViewResult>(ResultImageDao.Instance.GetAllByPid(result.Id));
+            }
+            List<GridViewColumn> gridViewColumns = new List<GridViewColumn>();
+            List<string> header = new() { "file_name", "order_index", "FileInfo" };
+            List<string> bdHeader = new() { "FileName", "OrderIndex", "FileInfo" };
+
+            if (view.listViewSide.View is GridView gridView)
+            {
+                view.LeftGridViewColumnVisibilitys.Clear();
+                gridView.Columns.Clear();
+                for (int i = 0; i < header.Count; i++)
+                    gridView.Columns.Add(new GridViewColumn() { Header = header[i], DisplayMemberBinding = new Binding(bdHeader[i]) });
+                view.listViewSide.ItemsSource = result.ViewResults;
+            }
+        }
+
+    }
+
+}
