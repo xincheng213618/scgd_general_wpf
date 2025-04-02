92a0b8cf1733b8c09a4992459cda3f928b871dcb
xincheng <xincheng213618@gmail.com>
2025-04-02 16:14:55 +0800

    update

--- Engine/ColorVision.Engine/Templates/Jsons/SFRFindROI/TemplateSFRFindROI.cs
+++ Engine/ColorVision.Engine/Templates/Jsons/SFRFindROI/TemplateSFRFindROI.cs
@@ -28,7 +28,7 @@ namespace ColorVision.Engine.Templates.Jsons.SFRFindROI
             EditTemplateJson = EditTemplateJson ?? new EditTemplateJson(Description);
             return EditTemplateJson;
         }
-        public string Description { get; set; } = "//blackmura 相关\r\nstruct BlackMuraParam\r\n{\r\n        float aa_threshold;  //阈值\r\n        int   erode_size;    //腐蚀去噪尺寸\r\n        int   min_aa_area;   //最小的发光区大小，判断pattern\r\n        int   aa_cut;  // aa区裁剪像素，负值表示外扩\r\n        int   display_w;  //pixel 屏幕分辨率\r\n        int   display_h;\r\n        double aa_size_w;  //aa区实际尺寸\r\n        double aa_size_h;\r\n        int   m_de;       //水平方向上的平均尺寸  ，显示器像素\r\n        int   n_de;       //垂直方向的平均尺寸\r\n        bool  rotate;     //是否旋转\r\n        int  poi_num_x;   //Uniformity 计算相关； block设置\r\n        int  poi_num_y;\r\n        int  poi_type;    //0，矩形 1 圆形\r\n};";
+        public string Description { get; set; } = "//sfr roi配置\r\nstruct SfrRoiParam\r\n{\r\n        float th;                 //二值化阈值      \r\n        float lowThreshold;       //边缘化低阈值 \r\n        float highThreshold;      //边缘化高阈值\r\n        float minLength;          //最短线段长度\r\n        int   roi_w;              //roi的宽和高      \r\n        int   roi_h;              //\r\n\r\n};";
 
         public override UserControl CreateUserControl() => new EditTemplateJson(Description);
 
