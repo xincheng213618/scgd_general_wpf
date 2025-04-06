b7f87e81419095186a8811bb59c12fc863472206
xincheng <xincheng213618@gmail.com>
2025-04-07 01:13:44 +0800

    update

--- Engine/ColorVision.Engine/Services/PhyCameras/Group/CalibrationResource.cs
+++ Engine/ColorVision.Engine/Services/PhyCameras/Group/CalibrationResource.cs
@@ -2,10 +2,12 @@
 using ColorVision.Common.Utilities;
 using ColorVision.Engine.Services.Core;
 using ColorVision.Engine.Services.Dao;
+using ColorVision.UI;
 using ColorVision.UI.Authorizations;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
+using System.Windows;
 
 namespace ColorVision.Engine.Services.PhyCameras.Group
 {
@@ -35,13 +37,29 @@ namespace ColorVision.Engine.Services.PhyCameras.Group
         }
         public RelayCommand OpenCommand { get; set; }
 
-
+        public RelayCommand EditCommand { get; set; }
 
         public CalibrationFileConfig Config { get; set; }
         public CalibrationResource(SysResourceModel sysResourceModel) : base(sysResourceModel)
         {
             CalibrationResources.Add(this);
             OpenCommand = new RelayCommand(a=> Open(),a => AccessControl.Check(PermissionMode.Administrator));
+            EditCommand = new RelayCommand(a => Edit(), a => AccessControl.Check(PermissionMode.Administrator));
+        }
+
+        public void Edit()
+        {
+            if (this.GetAncestor<PhyCamera>() is PhyCamera phyCamera)
+            {
+                if (Directory.Exists(phyCamera.Config.FileServerCfg.FileBasePath))
+                {
+                    string path = SysResourceModel.Value ?? string.Empty;
+
+                    string filepath = Path.Combine(phyCamera.Config.FileServerCfg.FileBasePath, phyCamera.Code, "cfg", path);
+                    AvalonEditWindow avalonEditWindow = new AvalonEditWindow(filepath) { Owner = Application.Current.GetActiveWindow(), WindowStartupLocation = WindowStartupLocation.CenterOwner };
+                    avalonEditWindow.ShowDialog();
+                }
+            }
         }
 
 
