a6faea4a4dfd93fe776fa175521d9840a00e208b
xincheng <xincheng213618@gmail.com>
2025-04-15 15:16:34 +0800

    update

--- Engine/ColorVision.Engine/Templates/Jsons/KB/ViewHandleKB.cs
+++ Engine/ColorVision.Engine/Templates/Jsons/KB/ViewHandleKB.cs
@@ -60,7 +60,7 @@ namespace ColorVision.Engine.Templates.Jsons.KB
             }
             catch (Exception ex)
             {
-
+                MessageBox.Show(ex.Message);
             }
         }
 
