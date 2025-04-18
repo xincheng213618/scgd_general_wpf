74bafcefe1090c2dc5bcdbe760b7eab5266c9730
xincheng <xincheng213618@gmail.com>
2025-04-18 16:49:04 +0800

    update

---
+++ Engine/ColorVision.Engine/Templates/Jsons/GhostQK/DeatilCommonDao.cs
@@ -0,0 +1,53 @@
+﻿using ColorVision.Engine.Interfaces;
+using ColorVision.Engine.MySql.ORM;
+using ColorVision.Engine.Services.Devices.Algorithm;
+using CVCommCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ColorVision.Engine.Templates.Jsons.GhostQK
+{
+
+    public class GhostView: IViewResult
+    {
+        public GhostView(DetailCommonModel detail)
+        {
+            Id = detail.Id;
+            PId = detail.PId;
+            Result = detail.ResultJson;
+        }
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("pid")]
+        public int PId { get; set; }
+
+        [Column("result")]
+        public string Result { get; set; }
+    }
+
+
+
+    [Table("t_scgd_algorithm_result_detail_common")]
+    public class DetailCommonModel : PKModel
+    {
+        [Column("pid")]
+        public int PId { get; set; }
+
+        [Column("result_json")]
+        public string ResultJson { get; set; }
+    }
+
+
+    public class DeatilCommonDao : BaseTableDao<DetailCommonModel>
+    {
+        public static DeatilCommonDao Instance { get; set; } = new DeatilCommonDao();
+        public DeatilCommonDao() : base("ot_scgd_algorithm_result_detail_common")
+        {
+        }
+    }
+
+
+
+}
---
+++ Engine/ColorVision.Engine/Templates/Jsons/GhostQK/MysqlGhostQK.cs
@@ -0,0 +1,15 @@
+﻿using ColorVision.Engine.MySql;
+
+namespace ColorVision.Engine.Templates.Jsons.GhostQK
+{
+    public class MysqlGhostQK : IMysqlCommand
+    {
+        public string GetMysqlCommandName() => "恢复GhostQK";
+
+        public string GetRecover()
+        {
+            string t_scgd_sys_dictionary_mod_master = "INSERT INTO `t_scgd_sys_dictionary_mod_master` (`id`, `code`, `name`, `p_type`, `pid`, `mod_type`, `cfg_json`, `create_date`, `is_enable`, `is_delete`, `remark`, `tenant_id`) VALUES (38, 'ghost_qk', '鬼影', 1, NULL, 7, '{}', '2024-04-28 11:38:32', 1, 0, NULL, 0);";
+            return t_scgd_sys_dictionary_mod_master;
+        }
+    }
+}
