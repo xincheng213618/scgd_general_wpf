f699b84712970aa2673fcb4ec5d0adec7362319c
xincheng <xincheng213618@gmail.com>
2025-04-29 12:15:26 +0800

    增加畸变2

---
+++ Engine/ColorVision.Engine/Templates/Jsons/Distortion2/GhostDetectionConfig.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorVision.Engine.Templates.Jsons.Distortion2
+{
+    public class GhostDetectionConfig
+    {
+        public VOtherConfig vOther { get; set; }
+        public BrightConfig Bright { get; set; }
+        public GhostConfig Ghost { get; set; }
+
+        static public string getDefaultJson()
+        {
+            var config = new GhostDetectionConfig
+            {
+                vOther = new VOtherConfig
+                {
+                    Debug = false,
+                    debugPath = "Result\\",
+                    showMinGain = 60,
+                    showMaxGain = 0.8f
+                },
+                Bright = new BrightConfig
+                {
+                    thresholdMin = 30000,
+                    thresholdMax = 40000,
+                    thresholdStep = 1000,
+                    brightNumX = 3,
+                    brightNumY = 3,
+                    patternType = 0,
+                    outRectSizeMin = 60,
+                    outRectSizeRate = 5,
+                    erodeKernel = 3
+                },
+                Ghost = new GhostConfig
+                {
+                    ingoreCheckMixBright = new List<bool> { false, false, false, false, true, false, false, false, false },
+                    thresholdMin = 50,
+                    thresholdMax = 500,
+                    thresholdStep = 10,
+                    outRectSizeMin = 100,
+                    outRectSizeRate = 7.3f,
+                    minGary = -1,
+                    garyRate = 1,
+                    erodeKernel = 3,
+                    erodeTime = 5,
+                    distanceToBright = 100
+                }
+
+            };
+
+            return JsonConvert.SerializeObject(config);
+        }
+    }
+}
