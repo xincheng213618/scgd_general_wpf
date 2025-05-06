781fb94c7efd7a351aa79448e67dfbceda994653
xincheng <xincheng213618@gmail.com>
2025-05-06 16:03:57 +0800

    update

--- Engine/ColorVision.Engine/Services/Devices/Camera/Dao/TemperatureChartWindow.xaml.cs
+++ Engine/ColorVision.Engine/Services/Devices/Camera/Dao/TemperatureChartWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using ColorVision.Engine.Services.Dao;
 using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
+using Microsoft.Win32;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,6 +34,8 @@ namespace ColorVision.Engine.Services.Devices.Camera.Dao
 
         private void Window_Initialized(object sender, EventArgs e)
         {
+
+            LiveCharts.Configure(config => config.HasGlobalSKTypeface(SKFontManager.Default.MatchCharacter('汉')));
             if (data!=null && data.Count > 0)
             {
                 // 设置窗口标题
@@ -75,7 +80,31 @@ namespace ColorVision.Engine.Services.Devices.Camera.Dao
 
         private void ButtonExport_Click(object sender, RoutedEventArgs e)
         {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv",
+                Title = "Save as CSV",
+                FileName =  DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".csv" // 设置默认文件名
+            };
 
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                var filePath = saveFileDialog.FileName;
+                var csv = new StringBuilder();
+
+                // 添加CSV文件头
+                csv.AppendLine("TempValue,PwmValue,CreateDate");
+
+                // 添加数据行
+                foreach (var item in data)
+                {
+                    var line = $"{item.TempValue},{item.PwmValue},{item.CreateDate?.ToString("yyyy-MM-dd HH:mm:ss")},{item.RescourceId}";
+                    csv.AppendLine(line);
+                }
+
+                // 写入文件
+                File.WriteAllText(filePath, csv.ToString(), Encoding.UTF8);
+            }
         }
     }
 }
