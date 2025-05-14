215cfb90b7d7c0d86f7b1f8a8e17d442a2a680e4
xincheng <xincheng213618@gmail.com>
2025-05-14 14:28:59 +0800

    更新版本至1.2.4.11，新增AA区算法功能
    - 更新CHANGELOG.md，增加版本1.2.4.11信息。
    - 在Directory.Build.props中更新版本前缀。
    - 在AlgorithmAAFindPoints.cs中实现寻找AA区算法。
    - 在DeatilCommonDao.cs中新增CameraFOV和DFovView类。
    - 在DisplayAAFindPoints.xaml中定义用户界面控件。
    - 在DisplayAAFindPoints.xaml.cs中实现用户控件逻辑。
    - 在GhostDetectionConfig.cs中新增命名空间定义。
    - 在MenuAAFindPoints.cs中新增“寻找AA区”菜单项。
    - 在MysqlAAFindPoints.cs中实现与MySQL的交互命令。
    - 在TemplateAAFindPoints.cs中实现模板管理功能。
    - 在ViewHandleDFOV.cs中注释掉DFOV视图处理相关代码。

---
+++ Engine/ColorVision.Engine/Templates/Jsons/AAFindPoints/GhostDetectionConfig.cs
@@ -0,0 +1,4 @@
+﻿namespace ColorVision.Engine.Templates.Jsons.AAFindPoints
+{
+
+}
---
+++ Engine/ColorVision.Engine/Templates/Jsons/AAFindPoints/TemplateAAFindPoints.cs
@@ -0,0 +1,63 @@
+﻿using ColorVision.Engine.MySql;
+using log4net;
+using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace ColorVision.Engine.Templates.Jsons.AAFindPoints
+{
+
+    public class TAAFindPointsParam : TemplateJsonParam
+    {
+        private static ILog log = LogManager.GetLogger(nameof(TAAFindPointsParam));
+
+
+        public TAAFindPointsParam() : base()
+        {
+        }
+
+        public TAAFindPointsParam(TemplateJsonModel templateJsonModel) : base(templateJsonModel)
+        {
+
+        }
+
+
+    }
+
+    public class TemplateAAFindPoints : ITemplateJson<TAAFindPointsParam>, IITemplateLoad
+    {
+        public static ObservableCollection<TemplateModel<TAAFindPointsParam>> Params { get; set; } = new ObservableCollection<TemplateModel<TAAFindPointsParam>>();
+
+        public TemplateAAFindPoints()
+        {
+            Title = "寻找AA区模板管理";
+            Code = "ARVR.AA.FindPoints";
+            Name = "寻找AA区";
+            TemplateDicId = 41;
+            TemplateParams = Params;
+            IsUserControl = true;
+        }
+
+        public override void SetUserControlDataContext(int index)
+        {
+            EditTemplateJson.SetParam(TemplateParams[index].Value);
+        }
+        public EditTemplateJson EditTemplateJson { get; set; }
+
+        public override UserControl GetUserControl()
+        {
+            EditTemplateJson = EditTemplateJson ?? new EditTemplateJson(Description);
+            return EditTemplateJson;
+        }
+        public string Description { get; set; } = "";
+
+        public override UserControl CreateUserControl() => new EditTemplateJson(Description);
+        public override IMysqlCommand? GetMysqlCommand() => new MysqlAAFindPoints();
+
+    }
+
+
+
+
+}
