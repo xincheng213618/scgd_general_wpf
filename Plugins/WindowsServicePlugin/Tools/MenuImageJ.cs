3265a8c7041272bf20871c7144a493ceddd26e5d
xincheng <xincheng213618@gmail.com>
2025-05-20 16:58:24 +0800

    update

--- Plugins/WindowsServicePlugin/Tools/MenuImageJ.cs
+++ Plugins/WindowsServicePlugin/Tools/MenuImageJ.cs
@@ -1,13 +1,19 @@
 ﻿#pragma warning disable SYSLIB0014
 using ColorVision.Common.MVVM;
+using ColorVision.Engine.Media;
+using ColorVision.ImageEditor;
+using ColorVision.ImageEditor.Draw;
+using ColorVision.Net;
 using ColorVision.Themes.Controls;
 using ColorVision.UI;
 using ColorVision.UI.Menus;
 using log4net;
+using Microsoft.Extensions.Configuration.UserSecrets;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 
@@ -40,7 +46,64 @@ namespace WindowsServicePlugin.Tools
             };
         }
     }
-    
+    public static class PathHelper
+    {
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int GetShortPathName(
+            string lpszLongPath,
+            System.Text.StringBuilder lpszShortPath,
+            int cchBuffer);
+
+        public static string GetShortPath(string longPath)
+        {
+            var shortPath = new System.Text.StringBuilder(260);
+            GetShortPathName(longPath, shortPath, shortPath.Capacity);
+            return shortPath.ToString();
+        }
+    }
+
+
+
+    public class ImageViewExTension: IImageContentMenu
+    {
+
+
+        public List<MenuItemMetadata> GetContextMenuItems(ImageViewConfig config)
+        {
+            if (!File.Exists(ImageJConfig.Instance.ImageJPath)) return new List<MenuItemMetadata>();
+            RelayCommand relayCommand = new RelayCommand(a =>
+            {
+                string shortFilePath = string.Empty;
+                if (CVFileUtil.IsCIEFile(config.FilePath))
+                {
+                    VExportCIE vExportCIE = new VExportCIE(config.FilePath);
+                    VExportCIE.SaveToTif(vExportCIE);
+                    shortFilePath = PathHelper.GetShortPath(vExportCIE.CoverFilePath);
+                }
+                else
+                {
+                    shortFilePath = PathHelper.GetShortPath(config.FilePath);
+                }
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = ImageJConfig.Instance.ImageJPath,
+                    Arguments = $"\"{shortFilePath}\"",
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                Process.Start(startInfo);
+            });
+            MenuItemMetadata menuItemMetadata = new MenuItemMetadata() { GuidId = "ImageJ", Order = 500, Header = "通过ImageJ打开", Command = relayCommand };
+            return new List<MenuItemMetadata>() { menuItemMetadata };
+        }
+    }
+
+
+
+
+
+
     public class MenuImageJ : MenuItemBase
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(MenuImageJ));
