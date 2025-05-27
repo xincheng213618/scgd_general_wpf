fdb9089925201f751161404a9419635a32d6da0c
xincheng <xincheng213618@gmail.com>
2025-05-27 19:25:14 +0800

    增加hex编辑器

--- UI/ColorVision.Solution/Editor/TextEditor.cs
+++ UI/ColorVision.Solution/Editor/TextEditor.cs
@@ -2,6 +2,8 @@
 using ColorVision.UI.PropertyEditor;
 using System.IO;
 using System.Windows.Controls;
+using WpfHexaEditor.Core;
+using WpfHexaEditor.Core.MethodExtention;
 
 namespace ColorVision.Solution
 {
@@ -16,6 +18,24 @@ namespace ColorVision.Solution
             if (File.Exists(filePath))
             {
                 return new AvalonEditControll(filePath);
+            }
+            return null;
+        }
+    }  
+    // 标记本类支持的扩展名，并设为默认
+    [EditorForExtension(".txt|.cs|.json|.java|.go|.md|.py|.dat|.js|.xml|.xaml|.cpp|.c|.bat|.sql|.css|.ps1", isDefault: true)]
+    public class HexEditor : EditorBase
+    {
+        public override string Name => "文本编辑器";
+
+        public override Control? Open(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                var HexEditor = new WpfHexaEditor.HexEditor();
+                HexEditor.PreloadByteInEditorMode = PreloadByteInEditor.MaxVisibleLineExtended;
+                HexEditor.FileName = filePath;
+                return HexEditor;
             }
             return null;
         }
