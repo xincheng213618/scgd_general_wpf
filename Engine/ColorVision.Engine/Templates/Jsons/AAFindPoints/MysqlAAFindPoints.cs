b660f6f365eb85ff62391211d4914df276f63daf
xincheng <xincheng213618@gmail.com>
2025-05-28 13:41:39 +0800

    update

--- Engine/ColorVision.Engine/Templates/Jsons/AAFindPoints/MysqlAAFindPoints.cs
+++ Engine/ColorVision.Engine/Templates/Jsons/AAFindPoints/MysqlAAFindPoints.cs
@@ -4,11 +4,11 @@ namespace ColorVision.Engine.Templates.Jsons.AAFindPoints
 {
     public class MysqlAAFindPoints : IMysqlCommand
     {
-        public string GetMysqlCommandName() => "恢复AA布点";
+        public string GetMysqlCommandName() => "恢复AA发光区检测";
 
         public string GetRecover()
         {
-            string t_scgd_sys_dictionary_mod_master = "INSERT INTO `cv`.`t_scgd_sys_dictionary_mod_master` (`id`, `code`, `name`, `p_type`, `pid` , `mod_type`, `cfg_json`, `version`, `create_date`, `is_enable` , `is_delete`, `remark`, `tenant_id`) VALUES (41, 'ARVR.AA.FindPoints', 'AA布点', 1, NULL , 7, '{\"pattern\": 1, \"MaskRect\": {\"h\": 0, \"w\": 0, \"x\": 0, \"y\": 0, \"enable\": false}, \"debugCfg\": {\"Debug\": false, \"debugPath\": \"Result\\\\\\\\\", \"debugImgResize\": 2}, \"erodeTime\": 1, \"nummber_x\": 3, \"nummber_y\": 3, \"threshold\": 10000, \"brightRate\": 0.1, \"offset_top\": 0, \"ExactCorner\": {\"edge\": 10, \"cutWidth\": 400, \"qualityLevel\": 0.04}, \"erodeKernel\": 3, \"offset_left\": 0, \"offset_right\": 0, \"offset_bottom\": 0}', '1.0', '2024-04-28 11:38:32', 1 , 0, NULL, 0) ON DUPLICATE KEY UPDATE `code` = VALUES(`code`), `name` = VALUES(`name`), `p_type` = VALUES(`p_type`), `pid` = VALUES(`pid`), `mod_type` = VALUES(`mod_type`) , `cfg_json` = VALUES(`cfg_json`), `version` = VALUES(`version`), `create_date` = VALUES(`create_date`), `is_enable` = VALUES(`is_enable`), `is_delete` = VALUES(`is_delete`) , `remark` = VALUES(`remark`), `tenant_id` = VALUES(`tenant_id`);";
+            string t_scgd_sys_dictionary_mod_master = "INSERT INTO `t_scgd_sys_dictionary_mod_master` (`id`, `code`, `name`, `p_type`, `pid` , `mod_type`, `cfg_json`, `version`, `create_date`, `is_enable` , `is_delete`, `remark`, `tenant_id`) VALUES (42, 'FindLightArea', 'AA发光区检测', 1, NULL , 7, '{\"pattern\": 1, \"MaskRect\": {\"h\": 0, \"w\": 0, \"x\": 0, \"y\": 0, \"enable\": false}, \"debugCfg\": {\"Debug\": false, \"debugPath\": \"Result\\\\\\\\\", \"debugImgResize\": 2}, \"erodeTime\": 1, \"nummber_x\": 3, \"nummber_y\": 3, \"threshold\": 10000, \"brightRate\": 0.1, \"offset_top\": 0, \"ExactCorner\": {\"edge\": 10, \"cutWidth\": 400, \"qualityLevel\": 0.04}, \"erodeKernel\": 3, \"offset_left\": 0, \"offset_right\": 0, \"offset_bottom\": 0}', 'AA', '2024-04-28 11:38:32', 1 , 0, NULL, 0) ON DUPLICATE KEY UPDATE `code` = VALUES(`code`), `name` = VALUES(`name`), `p_type` = VALUES(`p_type`), `pid` = VALUES(`pid`), `mod_type` = VALUES(`mod_type`) , `cfg_json` = VALUES(`cfg_json`), `version` = VALUES(`version`), `create_date` = VALUES(`create_date`), `is_enable` = VALUES(`is_enable`), `is_delete` = VALUES(`is_delete`) , `remark` = VALUES(`remark`), `tenant_id` = VALUES(`tenant_id`);";
             return t_scgd_sys_dictionary_mod_master;
         }
     }
---
+++ Engine/ColorVision.Engine/Templates/Jsons/BuildPOIAA/TemplateBuildPOIAA.cs
@@ -0,0 +1,61 @@
+﻿using ColorVision.Engine.MySql;
+using log4net;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace ColorVision.Engine.Templates.Jsons.BuildPOIAA
+{
+
+    public class TAAFindPointsParam : TemplateJsonParam
+    {
+        private static ILog log = LogManager.GetLogger(nameof(TAAFindPointsParam));
+
+
+        public TAAFindPointsParam() : base()
+        {
+        }
+
+        public TAAFindPointsParam(TemplateJsonModel templateJsonModel) : base(templateJsonModel)
+        {
+
+        }
+
+
+    }
+
+    public class TemplateBuildPOIAA : ITemplateJson<TAAFindPointsParam>, IITemplateLoad
+    {
+        public static ObservableCollection<TemplateModel<TAAFindPointsParam>> Params { get; set; } = new ObservableCollection<TemplateModel<TAAFindPointsParam>>();
+
+        public TemplateBuildPOIAA()
+        {
+            Title = "寻找AA区模板管理";
+            Code = "BuildPOI";
+            Name = "AA布点";
+            TemplateDicId = 41;
+            TemplateParams = Params;
+            IsUserControl = true;
+        }
+
+        public override void SetUserControlDataContext(int index)
+        {
+            EditTemplateJson.SetParam(TemplateParams[index].Value);
+        }
+        public EditTemplateJson EditTemplateJson { get; set; }
+
+        public override UserControl GetUserControl()
+        {
+            EditTemplateJson = EditTemplateJson ?? new EditTemplateJson(Description);
+            return EditTemplateJson;
+        }
+        public string Description { get; set; } = "";
+
+        public override UserControl CreateUserControl() => new EditTemplateJson(Description);
+        public override IMysqlCommand? GetMysqlCommand() => new MysqlAAFindPoints();
+
+    }
+
+
+
+
+}
