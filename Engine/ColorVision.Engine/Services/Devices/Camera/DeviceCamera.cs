36ffbaadf8578f9883eb6e420ec9d0c043bbf521
xincheng <xincheng213618@gmail.com>
2025-06-30 16:33:01 +0800

    update

--- Engine/ColorVision.Engine/Services/Devices/Camera/DeviceCamera.cs
+++ Engine/ColorVision.Engine/Services/Devices/Camera/DeviceCamera.cs
@@ -218,7 +218,6 @@ namespace ColorVision.Engine.Services.Devices.Camera
             {
                 if ( MessageBox.Show(Application.Current.GetActiveWindow(), "当前逻辑相机许可证过期，无法刷新设备列表，是否清空当前相机服务绑定的物理相机，然后在重试", "ColorVision",MessageBoxButton.YesNo) == MessageBoxResult.Yes)
                 {
-                    this.Config.Code = "";
                     Config.CameraCode = string.Empty;
                     Save();
                     return;
@@ -233,7 +232,6 @@ namespace ColorVision.Engine.Services.Devices.Camera
             {
                 if (MessageBox.Show(Application.Current.GetActiveWindow(), "当前逻辑相机离线，无法刷新设备列表，是否清空当前相机服务绑定的物理相机，然后在重试", "ColorVision", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
                 {
-                    this.Config.Code = "";
                     Config.CameraCode = string.Empty;
                     Save();
                     return;
