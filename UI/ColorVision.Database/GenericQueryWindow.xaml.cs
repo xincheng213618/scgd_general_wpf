e64c7ed0dc4efa7ae59dde166ab5cdb3120f2a62
xincheng <xincheng213618@gmail.com>
2025-08-21 16:54:23 +0800

    update

--- UI/ColorVision.Database/GenericQueryWindow.xaml.cs
+++ UI/ColorVision.Database/GenericQueryWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using ColorVision.Common.MVVM;
 using ColorVision.Common.Utilities;
 using ColorVision.Engine.MySql.ORM;
-using ColorVision.Engine.Services.Devices.Algorithm.Views;
 using ColorVision.UI;
 using log4net;
 using SqlSugar;
