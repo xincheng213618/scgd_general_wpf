b6be78ab882eb82901ab2e2d66fde8bf8617d282
xincheng <xincheng213618@gmail.com>
2025-09-04 14:42:36 +0800

    update

--- Projects/ProjectLUX/Services/SocketControl.cs
+++ Projects/ProjectLUX/Services/SocketControl.cs
@@ -2,6 +2,7 @@
 using ColorVision.SocketProtocol;
 using Dm.util;
 using ProjectLUX.PluginConfig;
+using SqlSugar;
 using System.Net.Sockets;
 using System.Windows;
 using static iText.Svg.SvgConstants;
@@ -24,13 +25,26 @@ namespace ProjectLUX.Services
             {
                 string code = list[0];
                 string sn = list[1];
+                sn = sn.TrimEnd(';');
+                if (code.startsWith("T00"))
+                {
 
-                List<string> strings = new List<string>();
-                strings.add("H0301");
-                strings.add(sn);
-                strings.add("00");
+                    // 取 code 的最后两位（如果 code 长度足够）
+                    string lastTwo = code.Length >= 2 ? code.Substring(code.Length - 2, 2) : code;
 
-                return string.Join(",", strings);
+                    // 拼接到 H030 上
+                    string h030x = "H03" + lastTwo;
+
+                    List<string> strings = new List<string>();
+                    strings.Add(h030x);
+                    strings.Add(sn);
+                    strings.Add("00");
+                    return string.Join(",", strings) + ";";
+                }
+                else
+                {
+                    return $"No right Code {code}";
+                }
             }
             else
             {
