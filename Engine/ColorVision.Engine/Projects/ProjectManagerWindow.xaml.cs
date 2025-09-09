3e88b1f64c93b9c1cfbcb04d6dcd2368564470f0
xincheng <xincheng213618@gmail.com>
2025-09-10 01:53:02 +0800

    update

--- Engine/ColorVision.Engine/Projects/ProjectManagerWindow.xaml.cs
+++ Engine/ColorVision.Engine/Projects/ProjectManagerWindow.xaml.cs
@@ -5,6 +5,18 @@ using System.Windows.Input;
 
 namespace ColorVision.Projects
 {
+    public class MenuProjectManager : MenuItemBase
+    {
+        public override string OwnerGuid => MenuItemConstants.Tool;
+        public override int Order => 9000;
+        public override string Header => Resources.ProjectManagerWindow;
+
+        [RequiresPermission(PermissionMode.Administrator)]
+        public override void Execute()
+        {
+            new ProjectManagerWindow() { Owner = Application.Current.GetActiveWindow(), WindowStartupLocation = WindowStartupLocation.CenterOwner }.ShowDialog();
+        }
+    }
     /// <summary>
     /// ProjectManagerWindow.xaml 的交互逻辑
     /// </summary>
@@ -21,7 +33,6 @@ namespace ColorVision.Projects
         private void Window_Initialized(object sender, System.EventArgs e)
         {
             this.DataContext = ProjectManager.GetInstance();
-            DefalutSearchComboBox.ItemsSource = new List<string>() { "ProjectKB", "ProjectBlackMura", "ProjectHeyuan", "ProjectShiyuan", "ProjectBase", "ProjectARVR" };
             ListViewProjects.SelectedIndex = 0;
             this.CommandBindings.Add(new CommandBinding(ApplicationCommands.Delete, (s, e) => ProjectManager.GetInstance().Projects[ListViewProjects.SelectedIndex].Delete(), (s, e) => e.CanExecute = ListViewProjects.SelectedIndex > -1));
         }
