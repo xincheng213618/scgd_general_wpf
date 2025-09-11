ad6cda5259539afda6e27379e227427fa66ae0b9
xincheng <xincheng213618@gmail.com>
2025-09-12 03:42:38 +0800

    update

--- Engine/ColorVision.Engine/Templates/Flow/FlowFile.cs
+++ Engine/ColorVision.Engine/Templates/Flow/FlowFile.cs
@@ -1,7 +1,11 @@
-﻿using ColorVision.Common.NativeMethods;
+﻿using ColorVision.Common.MVVM;
+using ColorVision.Common.NativeMethods;
 using ColorVision.Solution.FileMeta;
+using ColorVision.UI.Menus;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Windows;
 
 namespace ColorVision.Engine.Templates.Flow
 {
@@ -14,8 +18,14 @@ namespace ColorVision.Engine.Templates.Flow
             Name = FileInfo.Name;  
             Icon = FileIcon.GetFileIconImageSource(fileInfo.FullName);
         }
+
+        public override IEnumerable<MenuItemMetadata> GetMenuItems()
+        {
+            RelayCommand relayCommand = new RelayCommand(a => MessageBox.Show("@2222"));
+            MenuItemMetadata menuItemMetadata = new MenuItemMetadata() {  Header = "测试", Command = relayCommand };
+
+            return new List<MenuItemMetadata>(){ menuItemMetadata };
+        }
     }
-
-
 
 }
