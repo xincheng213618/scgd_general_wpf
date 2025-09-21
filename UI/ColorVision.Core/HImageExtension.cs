c2735cf56cd0ccb836756ae96eb2143ab41bf363
xincheng <xincheng213618@gmail.com>
2025-09-21 20:28:20 +0800

    update

--- UI/ColorVision.Core/HImageExtension.cs
+++ UI/ColorVision.Core/HImageExtension.cs
@@ -7,7 +7,7 @@ using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
-namespace ColorVision
+namespace ColorVision.Core
 {
     public static class HImageExtension
     {
