fd9bd441d48d569b6f0fb2dfc27fa8c52d7f593d
xincheng <xincheng213618@gmail.com>
2025-09-28 16:12:16 +0800

    update

--- Engine/ColorVision.Engine/Services/ServicesConfig.cs
+++ Engine/ColorVision.Engine/Services/ServicesConfig.cs
@@ -4,27 +4,6 @@ using System.Collections.Generic;
 
 namespace ColorVision.Engine.Services
 {
-    public class ServicesConfigProvider : IConfigSettingProvider
-    {
-        public IEnumerable<ConfigSettingMetadata> GetConfigSettings()
-        { 
-            return new List<ConfigSettingMetadata>
-            {
-                new ConfigSettingMetadata
-                {
-                    Name = Properties.Resources.IsRetorePlayControls,
-                    Description = Properties.Resources.IsRetorePlayControls,
-                    Group ="Engine",
-                    Order = 16,
-                    Type = ConfigSettingType.Bool,
-                    BindingName =nameof(DisPlayManagerConfig.IsRetore),
-                    Source = DisPlayManagerConfig.Instance,
-                }
-            };
-        }
-    }
-
-
     public class ServicesConfig : ViewModelBase, IConfig
     {
         public static ServicesConfig Instance => ConfigService.Instance.GetRequiredService<ServicesConfig>();
