21f563a039143f6e1e29583c2e0b46bea097ce99
xincheng <xincheng213618@gmail.com>
2025-10-07 01:56:53 +0800

    update

--- UI/ColorVision.ImageEditor/ImageViewModel.cs
+++ UI/ColorVision.ImageEditor/ImageViewModel.cs
@@ -4,21 +4,25 @@ using ColorVision.Common.Utilities;
 using ColorVision.ImageEditor.Draw;
 using ColorVision.ImageEditor.Draw.Ruler;
 using ColorVision.ImageEditor.Draw.Special;
-using ColorVision.ImageEditor.EditorTools.Rotate;
 using ColorVision.ImageEditor.EditorTools.FullScreen;
+using ColorVision.ImageEditor.EditorTools.Rotate;
 using ColorVision.UI;
+using ColorVision.UI.Menus;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace ColorVision.ImageEditor
 {
     public class ImageViewModel : ViewModelBase, IDisposable
     {
         public DrawEditorManager DrawEditorManager { get; set; } = new DrawEditorManager();
+
         public Guid Id { get; set; } = Guid.NewGuid();
 
         #region Commands
@@ -65,7 +69,6 @@ namespace ColorVision.ImageEditor
 
         #region Helper Classes
         private ImageTransformOperations _transformOperations;
-        private ImageContextMenuManager _contextMenuManager;
         private ImageKeyboardHandler _keyboardHandler;
         #endregion
 
@@ -122,13 +125,150 @@ namespace ColorVision.ImageEditor
 
             InitializeTools(zoombox, drawCanvas);
 
-
-            _contextMenuManager = new ImageContextMenuManager(this, drawCanvas, ContextMenu, ContextMenuProviders);
-            Image.ContextMenuOpening += _contextMenuManager.HandleContextMenuOpening;
+            Image.ContextMenuOpening += HandleContextMenuOpening;
             Image.ContextMenu = ContextMenu;
             ZoomboxSub.ContextMenu = ContextMenu;
             ZoomboxSub.LayoutUpdated += Zoombox1_LayoutUpdated;
         }
+
+
+        /// <summary>
+        /// 处理上下文菜单打开事件
+        /// </summary>
+        public void HandleContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+            ContextMenu.Items.Clear();
+
+            if (ImageEditMode)
+            {
+                Point MouseDownP = Mouse.GetPosition(Image);
+                var MouseVisual = Image.GetVisual<Visual>(MouseDownP);
+                Type type = MouseVisual.GetType();
+
+                if (MouseVisual is SelectEditorVisual selectEditorVisual && selectEditorVisual.GetVisual(MouseDownP) is ISelectVisual selectVisual)
+                {
+                    foreach (var provider in ContextMenuProviders)
+                    {
+                        if (provider.ContextType.IsAssignableFrom(selectVisual.GetType()))
+                        {
+                            var items = provider.GetContextMenuItems(this, selectVisual);
+                            foreach (var item in items)
+                                ContextMenu.Items.Add(item);
+                        }
+                    }
+                    foreach (var provider in ContextMenuProviders)
+                    {
+                        if (provider.ContextType.IsAssignableFrom(selectEditorVisual.GetType()))
+                        {
+                            var items = provider.GetContextMenuItems(this, selectEditorVisual);
+                            foreach (var item in items)
+                                ContextMenu.Items.Add(item);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var provider in ContextMenuProviders)
+                    {
+                        if (provider.ContextType.IsAssignableFrom(type))
+                        {
+                            var items = provider.GetContextMenuItems(this, MouseVisual);
+                            foreach (var item in items)
+                                ContextMenu.Items.Add(item);
+                        }
+                    }
+                }
+
+                if (ContextMenu.Items.Count == 0)
+                    CreateStandardContextMenu();
+            }
+            else
+            {
+                CreateStandardContextMenu();
+            }
+        }
+
+        /// <summary>
+        /// 创建标准上下文菜单
+        /// </summary>
+        public void CreateStandardContextMenu()
+        {
+            List<MenuItemMetadata> MenuItemMetadatas = new List<MenuItemMetadata>();
+            if (IImageOpen is IIEditorToolContextMenu contentMenuProvider)
+            {
+                MenuItemMetadatas.AddRange(contentMenuProvider.GetContextMenuItems());
+            }
+
+            foreach (var item in IEditorToolFactory.IIEditorToolContextMenus)
+            {
+                if (item is IImageOpen) continue;
+                MenuItemMetadatas.AddRange(item.GetContextMenuItems());
+            }
+
+
+            var iMenuItems = MenuItemMetadatas.OrderBy(item => item.Order).ToList();
+
+            // 递归创建菜单结构
+            void CreateMenu(MenuItem parentMenuItem, string OwnerGuid)
+            {
+                var iMenuItems1 = iMenuItems.FindAll(a => a.OwnerGuid == OwnerGuid).OrderBy(a => a.Order).ToList();
+                for (int i = 0; i < iMenuItems1.Count; i++)
+                {
+                    var iMenuItem = iMenuItems1[i];
+                    string GuidId = iMenuItem.GuidId ?? Guid.NewGuid().ToString();
+                    MenuItem menuItem = new()
+                    {
+                        Header = iMenuItem.Header,
+                        Icon = iMenuItem.Icon,
+                        InputGestureText = iMenuItem.InputGestureText,
+                        Command = iMenuItem.Command,
+                        Tag = iMenuItem,
+                        IsChecked = iMenuItem.IsChecked ?? false,
+                        Visibility = iMenuItem.Visibility,
+                    };
+
+                    CreateMenu(menuItem, GuidId);
+                    if (i > 0 && iMenuItem.Order - iMenuItems1[i - 1].Order > 4 && iMenuItem.Visibility == Visibility.Visible)
+                    {
+                        parentMenuItem.Items.Add(new Separator());
+                    }
+                    parentMenuItem.Items.Add(menuItem);
+                }
+                foreach (var item in iMenuItems1)
+                {
+                    iMenuItems.Remove(item);
+                }
+            }
+
+            // 创建顶级菜单
+            var iMenuItemMetas = MenuItemMetadatas
+                .Where(item => item.OwnerGuid == MenuItemConstants.Menu && item.Visibility == Visibility.Visible)
+                .OrderBy(item => item.Order)
+                .ToList();
+
+            for (int i = 0; i < iMenuItemMetas.Count; i++)
+            {
+                MenuItemMetadata menuItemMeta = iMenuItemMetas[i];
+                MenuItem menuItem = new()
+                {
+                    Header = menuItemMeta.Header,
+                    Command = menuItemMeta.Command,
+                    Icon = menuItemMeta.Icon,
+                    InputGestureText = menuItemMeta.InputGestureText,
+                    IsChecked = menuItemMeta.IsChecked ?? false,
+                };
+                if (menuItemMeta.GuidId != null)
+                    CreateMenu(menuItem, menuItemMeta.GuidId);
+                if (i > 0 && menuItemMeta.Order - iMenuItemMetas[i - 1].Order > 4)
+                    _contextMenu.Items.Add(new Separator());
+
+                _contextMenu.Items.Add(menuItem);
+            }
+        }
+
+
+
+
 
         double oldMax;
         private void Zoombox1_LayoutUpdated(object? sender, EventArgs e)
