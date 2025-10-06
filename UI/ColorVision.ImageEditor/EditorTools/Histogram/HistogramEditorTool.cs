115f8f84b65c86f2e7c21ed57815bc1ca1db0a8f
xincheng <xincheng213618@gmail.com>
2025-10-07 05:29:48 +0800

    update

---
+++ UI/ColorVision.ImageEditor/EditorTools/Histogram/HistogramEditorTool.cs
@@ -0,0 +1,37 @@
+using ColorVision.Common.MVVM;
+using ColorVision.Common.Utilities;
+using ColorVision.UI;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace ColorVision.ImageEditor.EditorTools.Histogram
+{
+    public record class HistogramEditorTool(EditorContext EditorContext) : IEditorTool
+    {
+        public ToolBarLocal ToolBarLocal => ToolBarLocal.Right;
+        public string? GuidId => "Histogram";
+        public int Order { get; set; } = 10;
+
+        public object Icon { get; set; } = IEditorToolFactory.TryFindResource("DIHistogram");
+
+        public ICommand? Command { get; set; } = new RelayCommand(_ =>
+        {
+            if (EditorContext.DrawCanvas.Source is not BitmapSource bitmapSource) return;
+
+            var (redHistogram, greenHistogram, blueHistogram) = ImageUtils.RenderHistogram(bitmapSource);
+            HistogramChartWindow window;
+            if (bitmapSource.Format.Masks.Count == 1)
+            {
+                window = new HistogramChartWindow(redHistogram);
+            }
+            else
+            {
+                window = new HistogramChartWindow(redHistogram, greenHistogram, blueHistogram);
+            }
+
+            window.Owner = Application.Current.GetActiveWindow();
+            window.Show();
+        });
+    }
+}
