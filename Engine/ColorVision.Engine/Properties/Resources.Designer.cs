580d86be3c66036d9df7e9dcb9c82cf446f57042
xincheng <xincheng213618@gmail.com>
2025-11-06 14:07:11 +0800

    update

--- Engine/ColorVision.Engine/Properties/Resources.Designer.cs
+++ Engine/ColorVision.Engine/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ColorVision.Engine.Properties {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -462,6 +462,15 @@ namespace ColorVision.Engine.Properties {
         public static string Capture {
             get {
                 return ResourceManager.GetString("Capture", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 拍照 的本地化字符串。
+        /// </summary>
+        public static string CaptureImage {
+            get {
+                return ResourceManager.GetString("CaptureImage", resourceCulture);
             }
         }
         
@@ -2752,6 +2761,15 @@ namespace ColorVision.Engine.Properties {
         }
         
         /// <summary>
+        ///   查找类似 停止执行 的本地化字符串。
+        /// </summary>
+        public static string StopExecution {
+            get {
+                return ResourceManager.GetString("StopExecution", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   查找类似 停止流程 的本地化字符串。
         /// </summary>
         public static string StopProcess {
@@ -3117,20 +3135,6 @@ namespace ColorVision.Engine.Properties {
         public static string WorkflowEngineTemplateManagement {
             get {
                 return ResourceManager.GetString("WorkflowEngineTemplateManagement", resourceCulture);
-            }
-        }
-        public static string StopExecution
-        {
-            get
-            {
-                return ResourceManager.GetString("StopExecution", resourceCulture);
-            }
-        }
-        public static string CaptureImage
-        {
-            get
-            {
-                return ResourceManager.GetString("CaptureImage", resourceCulture);
             }
         }
     }
