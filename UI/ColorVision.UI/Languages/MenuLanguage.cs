461d6f66d06c589e28b3dc645727757f5801f5e1
xincheng <xincheng213618@gmail.com>
2025-11-10 23:24:54 +0800

    update

--- UI/ColorVision.UI/Languages/MenuLanguage.cs
+++ UI/ColorVision.UI/Languages/MenuLanguage.cs
@@ -1,57 +1,57 @@
-﻿//using ColorVision.Common.MVVM;
-//using ColorVision.UI.Menus;
+﻿using ColorVision.Common.MVVM;
+using ColorVision.UI.Menus;
 
-//namespace ColorVision.UI.Languages
-//{      
-//    //这里隐藏掉，因为我认为频繁切换语言是不需要的
-
-
-//    public class MenuLanguage : MenuItemBase
-//    {
-//        public override string OwnerGuid => MenuItemConstants.Tool;
-//        public override int Order => 1000;
-//        public override string Header => Properties.Resources.MenuLanguage;
-//        public override string InputGestureText => "Ctrl + Shift + T";
-//    }
+namespace ColorVision.UI.Languages
+{
+    //这里隐藏掉，因为我认为频繁切换语言是不需要的
 
 
-//    public class MenuThemeProvider : IMenuItemProvider
-//    {
-//        public IEnumerable<MenuItemMetadata> GetMenuItems()
-//        {
-//            List<MenuItemMetadata> menuItemMetas = new List<MenuItemMetadata>();
-//            int i = 1;
-//            foreach (var item in LanguageManager.Current.Languages)
-//            {
-//                i++;
-//                RelayCommand relayCommand = new RelayCommand(a =>
-//                {
-//                    string temp = Thread.CurrentThread.CurrentUICulture.Name;
-//                    LanguageConfig.Instance.UICulture = item;
-//                    bool sucess = LanguageManager.Current.LanguageChange(item);
-//                    if (!sucess)
-//                    {
-//                        LanguageConfig.Instance.UICulture = temp;
-//                    }
-
-//                    MenuManager.GetInstance().RefreshMenuItemsByGuid(nameof(MenuLanguage));
-//                });
+    public class MenuLanguage : MenuItemBase
+    {
+        public override string OwnerGuid => MenuItemConstants.Menu;
+        public override int Order => 1000;
+        public override string Header => Properties.Resources.MenuLanguage;
+        public override string InputGestureText => "Ctrl + Shift + T";
+    }
 
 
-//                MenuItemMetadata menuItemMeta = new MenuItemMetadata
-//                {
-//                    OwnerGuid = nameof(MenuLanguage),
-//                    GuidId = item,
-//                    Header = LanguageManager.keyValuePairs.TryGetValue(item, out string value) ? value : item,
-//                    Icon = null, // Set your icon here if needed
-//                    Order = i,
-//                    Command = relayCommand,
-//                    IsChecked = Thread.CurrentThread.CurrentUICulture.Name == item
-//                };
-//                menuItemMetas.Add(menuItemMeta);
-//            }
+    public class MenuThemeProvider : IMenuItemProvider
+    {
+        public IEnumerable<MenuItemMetadata> GetMenuItems()
+        {
+            List<MenuItemMetadata> menuItemMetas = new List<MenuItemMetadata>();
+            int i = 1;
+            foreach (var item in LanguageManager.Current.Languages)
+            {
+                i++;
+                RelayCommand relayCommand = new RelayCommand(a =>
+                {
+                    string temp = Thread.CurrentThread.CurrentUICulture.Name;
+                    LanguageConfig.Instance.UICulture = item;
+                    bool sucess = LanguageManager.Current.LanguageChange(item);
+                    if (!sucess)
+                    {
+                        LanguageConfig.Instance.UICulture = temp;
+                    }
 
-//            return menuItemMetas;
-//        }
-//    }
-//}
+                    MenuManager.GetInstance().RefreshMenuItemsByGuid(nameof(MenuLanguage));
+                });
+
+
+                MenuItemMetadata menuItemMeta = new MenuItemMetadata
+                {
+                    OwnerGuid = nameof(MenuLanguage),
+                    GuidId = item,
+                    Header = LanguageManager.keyValuePairs.TryGetValue(item, out string value) ? value : item,
+                    Icon = null, // Set your icon here if needed
+                    Order = i,
+                    Command = relayCommand,
+                    IsChecked = Thread.CurrentThread.CurrentUICulture.Name == item
+                };
+                menuItemMetas.Add(menuItemMeta);
+            }
+
+            return menuItemMetas;
+        }
+    }
+}
