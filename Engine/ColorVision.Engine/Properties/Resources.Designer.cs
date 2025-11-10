3b86c89520306b23a4b965705b9f5d97e38c2685
xincheng <xincheng213618@gmail.com>
2025-11-11 01:50:54 +0800

    update

--- Engine/ColorVision.Engine/Properties/Resources.Designer.cs
+++ Engine/ColorVision.Engine/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ColorVision.Engine.Properties {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1452,6 +1452,15 @@ namespace ColorVision.Engine.Properties {
         public static string ConnectType {
             get {
                 return ResourceManager.GetString("ConnectType", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 连续测量 的本地化字符串。
+        /// </summary>
+        public static string ContinuousMeasurement {
+            get {
+                return ResourceManager.GetString("ContinuousMeasurement", resourceCulture);
             }
         }
         
@@ -6980,13 +6989,5 @@ namespace ColorVision.Engine.Properties {
                 return ResourceManager.GetString("WorkflowTemplate", resourceCulture);
             }
         }
-        public static string ContinuousMeasurement
-        {
-            get
-            {
-                return ResourceManager.GetString("ContinuousMeasurement", resourceCulture);
-            }
-        }
-        
     }
 }
