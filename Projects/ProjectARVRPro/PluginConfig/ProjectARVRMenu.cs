0caa8d40a92e9524e6c9c8ccc10ba2b29f23f0a0
xincheng <xincheng213618@gmail.com>
2025-11-18 04:12:33 +0800

    update

--- Projects/ProjectARVRPro/PluginConfig/ProjectARVRMenu.cs
+++ Projects/ProjectARVRPro/PluginConfig/ProjectARVRMenu.cs
@@ -11,7 +11,7 @@ namespace ProjectARVRPro.PluginConfig
         public override string OwnerGuid => MenuItemConstants.Tool;
 
         public override int Order => 100;
-        public override string Header => "ProjectARVRPro";
+        public override string Header => "ARVRPro";
 
         public override void Execute()
         {
