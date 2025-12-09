2525e944609e7d230caafa6b7ebfeb2a771913ab
xincheng <xincheng213618@gmail.com>
2025-12-09 12:52:38 +0800

    update

--- Engine/ColorVision.Engine/Services/Devices/SMU/Configs/ConfigSMU.cs
+++ Engine/ColorVision.Engine/Services/Devices/SMU/Configs/ConfigSMU.cs
@@ -5,12 +5,16 @@ namespace ColorVision.Engine.Services.Devices.SMU.Configs
     public class ConfigSMU : DeviceServiceConfig
     {
         [DisplayName("Is4Wire")]
-        public bool Is4Wire { get => _is4Wire; set { _is4Wire = value; OnPropertyChanged(); } }
-        private bool _is4Wire;
+        public bool Is4Wire { get => _Is4Wire; set { _Is4Wire = value; OnPropertyChanged(); } }
+        private bool _Is4Wire;
 
         [DisplayName("IsFront")]
         public bool IsFront { get => _IsFront; set { _IsFront = value; OnPropertyChanged(); } }
         private bool _IsFront;
+
+        [DisplayName("IsSrcA")]
+        public bool IsSrcA { get => _IsSrcA; set { _IsSrcA = value; OnPropertyChanged(); } }
+        private bool _IsSrcA;
 
         public bool IsNet { get => _IsNet; set { _IsNet = value; OnPropertyChanged(); } }
         private bool _IsNet;
