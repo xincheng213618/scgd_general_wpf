443b9dfeb19b83a0a7f783d98c073d3dc9f0833f
xincheng <xincheng213618@gmail.com>
2025-12-18 15:14:20 +0800

    update

--- Engine/ColorVision.Engine/Services/Devices/MQTTDeviceService.cs
+++ Engine/ColorVision.Engine/Services/Devices/MQTTDeviceService.cs
@@ -1,12 +1,10 @@
 ﻿using ColorVision.Common.Utilities;
-using ColorVision.Engine.Services.RC;
 using System.Windows;
 
 namespace ColorVision.Engine.Services.Devices
 {
     public class MQTTDeviceService<T> : MQTTServiceBase where T : BaseConfig
     {
-
 
         public event DeviceStatusChangedHandler DeviceStatusChanged;
 
@@ -28,24 +26,35 @@ namespace ColorVision.Engine.Services.Devices
                 _DeviceStatus = value;
 
                 Application.Current?.Dispatcher.BeginInvoke(() => DeviceStatusChanged?.Invoke(value));
-                OnPropertyChanged(); 
+                OnPropertyChanged(); OnPropertyChanged(nameof(DeviceStatusString));
             }
         }
 
         public int TryN { get; set; }
 
 
+
         private DeviceStatusType _DeviceStatus;
 
+        public string DeviceStatusString { get => _DeviceStatus.ToDescription(); set { } }
+
         public T Config { get; set; }
+
+        public override string SubscribeTopic { get => Config.SubscribeTopic; set { Config.SubscribeTopic = value; } }
+
+        public override string DeviceCode { get => Config.Code; set => Config.Code = value; }
+
+        public override string SendTopic { get => Config.SendTopic; set { Config.SendTopic = value; } }
+
+        public override int HeartbeatTime { get => Config.HeartbeatTime; set { Config.HeartbeatTime = value; OnPropertyChanged(); } }
+
+        public override string ServiceToken { get => Config.ServiceToken; set => Config.ServiceToken = value; }
 
         public MQTTDeviceService(T config) : base()
         {
             Config = config;
             SendTopic = Config.SendTopic;
             SubscribeTopic = Config.SubscribeTopic;
-            DeviceCode = Config.Code;
-            HeartbeatTime =Config.HeartbeatTime;    
         }
     }
 }
