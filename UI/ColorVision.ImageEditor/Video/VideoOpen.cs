51ebba097cb14bd02b9997dc81acbd45a981879f
xincheng <xincheng213618@gmail.com>
2026-02-22 03:58:02 +0800

    update

--- UI/ColorVision.ImageEditor/Video/VideoOpen.cs
+++ UI/ColorVision.ImageEditor/Video/VideoOpen.cs
@@ -312,7 +312,7 @@ namespace ColorVision.ImageEditor.Video
 
                         // Throttle slider/time updates to reduce UI overhead
                         int count = Interlocked.Increment(ref _uiUpdateCounter);
-                        if (count % 3 == 0)
+                        if (count % 60 == 0)
                         {
                             if (!_isDragging)
                             {
