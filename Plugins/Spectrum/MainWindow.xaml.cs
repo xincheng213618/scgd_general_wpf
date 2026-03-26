bf29fdb6704799e86a6fad05c83d8e736ca0ac9e
xincheng <xincheng213618@gmail.com>
2026-03-26 16:22:41 +0800

    update

--- Plugins/Spectrum/MainWindow.xaml.cs
+++ Plugins/Spectrum/MainWindow.xaml.cs
@@ -107,8 +107,8 @@ namespace Spectrum
             image.Source = src1931?.ToBitmapSource();
             var spectrometerTypeNames = new Dictionary<SpectrometerType, string>
             {
-                { SpectrometerType.CMvSpectra, "显示 SP100" },
-                { SpectrometerType.LightModule, "显示 SP10" }
+                { SpectrometerType.CMvSpectra, "SP100" },
+                { SpectrometerType.LightModule, "SP10" }
             };
             ComboBoxSpectrometerType.ItemsSource = from e1 in Enum.GetValues(typeof(SpectrometerType)).Cast<SpectrometerType>()
                                                    select new KeyValuePair<SpectrometerType, string>(e1, spectrometerTypeNames.TryGetValue(e1, out var name) ? name : e1.ToString());
