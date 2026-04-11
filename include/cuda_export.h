c8625d3774b8b935f817b80ba8ff761eb20ab147
xincheng <xincheng213618@gmail.com>
2026-04-12 07:01:15 +0800

    fix

--- include/cuda_export.h
+++ include/cuda_export.h
@@ -16,3 +16,10 @@ extern "C" COLORVISIONCORE_API int M_Fusion(const char* fusionjson, HImage* outI
 extern "C" COLORVISIONCORE_API int CM_Fusion(const char* fusionjson, HImage* outImage);
 extern "C" COLORVISIONCORE_API int CM_Fusion_Async(const char* fusionjson, HImage* outImage);
 extern "C" COLORVISIONCORE_API int CM_Fusion_Batch(const char* batchjson, HImage* outImages, int* outCount);
+
+typedef void(__stdcall* CVNativeLogCallback)(int source, int level, const char* message);
+
+extern "C" COLORVISIONCORE_API void CM_SetLogCallback(CVNativeLogCallback callback);
+extern "C" COLORVISIONCORE_API void CM_SetLogEnabled(int enabled);
+extern "C" COLORVISIONCORE_API void CM_SetLogLevel(int level);
+extern "C" COLORVISIONCORE_API void CM_EnableNativeSink(int enabled);
