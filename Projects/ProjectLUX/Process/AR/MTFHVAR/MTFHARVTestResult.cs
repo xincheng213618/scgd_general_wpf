f69b4ab828c6971b88438bf48c57ee4875a3baf5
xincheng <xincheng213618@gmail.com>
2026-04-17 16:23:19 +0800

    update

--- Projects/ProjectLUX/Process/AR/MTFHVAR/MTFHARVTestResult.cs
+++ Projects/ProjectLUX/Process/AR/MTFHVAR/MTFHARVTestResult.cs
@@ -12,77 +12,77 @@ namespace ProjectLUX.Process.MTFHVAR
     public class MTFHARVTestResult : ViewModelBase
     {
      // 0F Center
-        public ObjectiveTestItem MTF0F_Center_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-1_H1"};
-        public ObjectiveTestItem MTF0F_Center_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-1_V1"};
-        public ObjectiveTestItem MTF0F_Center_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-1_H2" };
-        public ObjectiveTestItem MTF0F_Center_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-1_V2"};
-        public ObjectiveTestItem MTF0F_Center_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-1_horizontal" };
-        public ObjectiveTestItem MTF0F_Center_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-1_Vertical" };
+        public ObjectiveTestItem MTF0F_Center_H1 { get; set; } = new ObjectiveTestItem() { Name = "P1_H1"};
+        public ObjectiveTestItem MTF0F_Center_V1 { get; set; } = new ObjectiveTestItem() { Name = "P1_V1"};
+        public ObjectiveTestItem MTF0F_Center_H2 { get; set; } = new ObjectiveTestItem() { Name = "P1_H2" };
+        public ObjectiveTestItem MTF0F_Center_V2 { get; set; } = new ObjectiveTestItem() { Name = "P1_V2"};
+        public ObjectiveTestItem MTF0F_Center_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P1_horizontal" };
+        public ObjectiveTestItem MTF0F_Center_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P1_Vertical" };
         
         // 0.4F LeftUp
-        public ObjectiveTestItem MTF0_4F_LeftUp_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-2_H1" };
-        public ObjectiveTestItem MTF0_4F_LeftUp_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-2_V1" };
-        public ObjectiveTestItem MTF0_4F_LeftUp_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-2_H2" };
-        public ObjectiveTestItem MTF0_4F_LeftUp_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-2_V2" };
-        public ObjectiveTestItem MTF0_4F_LeftUp_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-2_horizontal" };
-        public ObjectiveTestItem MTF0_4F_LeftUp_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-2_Vertical" };
+        public ObjectiveTestItem MTF0_4F_LeftUp_H1 { get; set; } = new ObjectiveTestItem() { Name = "P2_H1" };
+        public ObjectiveTestItem MTF0_4F_LeftUp_V1 { get; set; } = new ObjectiveTestItem() { Name = "P2_V1" };
+        public ObjectiveTestItem MTF0_4F_LeftUp_H2 { get; set; } = new ObjectiveTestItem() { Name = "P2_H2" };
+        public ObjectiveTestItem MTF0_4F_LeftUp_V2 { get; set; } = new ObjectiveTestItem() { Name = "P2_V2" };
+        public ObjectiveTestItem MTF0_4F_LeftUp_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P2_horizontal" };
+        public ObjectiveTestItem MTF0_4F_LeftUp_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P2_Vertical" };
    
     // 0.4F RightUp
-        public ObjectiveTestItem MTF0_4F_RightUp_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-3_H1" };
-        public ObjectiveTestItem MTF0_4F_RightUp_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-3_V1" };
-        public ObjectiveTestItem MTF0_4F_RightUp_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-3_H2" };
-        public ObjectiveTestItem MTF0_4F_RightUp_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-3_V2" };
-        public ObjectiveTestItem MTF0_4F_RightUp_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-3_horizontal" };
-        public ObjectiveTestItem MTF0_4F_RightUp_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-3_Vertical" };
+        public ObjectiveTestItem MTF0_4F_RightUp_H1 { get; set; } = new ObjectiveTestItem() { Name = "P3_H1" };
+        public ObjectiveTestItem MTF0_4F_RightUp_V1 { get; set; } = new ObjectiveTestItem() { Name = "P3_V1" };
+        public ObjectiveTestItem MTF0_4F_RightUp_H2 { get; set; } = new ObjectiveTestItem() { Name = "P3_H2" };
+        public ObjectiveTestItem MTF0_4F_RightUp_V2 { get; set; } = new ObjectiveTestItem() { Name = "P3_V2" };
+        public ObjectiveTestItem MTF0_4F_RightUp_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P3_horizontal" };
+        public ObjectiveTestItem MTF0_4F_RightUp_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P3_Vertical" };
       
       // 0.4F RightDown
-      public ObjectiveTestItem MTF0_4F_RightDown_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-4_H1" };
-        public ObjectiveTestItem MTF0_4F_RightDown_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-4_V1" };
-        public ObjectiveTestItem MTF0_4F_RightDown_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-4_H2" };
-        public ObjectiveTestItem MTF0_4F_RightDown_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-4_V2" };
-        public ObjectiveTestItem MTF0_4F_RightDown_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-4_horizontal" };
-        public ObjectiveTestItem MTF0_4F_RightDown_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-4_Vertical" };
+      public ObjectiveTestItem MTF0_4F_RightDown_H1 { get; set; } = new ObjectiveTestItem() { Name = "P4_H1" };
+        public ObjectiveTestItem MTF0_4F_RightDown_V1 { get; set; } = new ObjectiveTestItem() { Name = "P4_V1" };
+        public ObjectiveTestItem MTF0_4F_RightDown_H2 { get; set; } = new ObjectiveTestItem() { Name = "P4_H2" };
+        public ObjectiveTestItem MTF0_4F_RightDown_V2 { get; set; } = new ObjectiveTestItem() { Name = "P4_V2" };
+        public ObjectiveTestItem MTF0_4F_RightDown_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P4_horizontal" };
+        public ObjectiveTestItem MTF0_4F_RightDown_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P4_Vertical" };
 
         // 0.4F LeftDown
-        public ObjectiveTestItem MTF0_4F_LeftDown_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-5_H1" };
-        public ObjectiveTestItem MTF0_4F_LeftDown_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-5_V1" };
-        public ObjectiveTestItem MTF0_4F_LeftDown_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-5_H2" };
-        public ObjectiveTestItem MTF0_4F_LeftDown_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-5_V2" };
-        public ObjectiveTestItem MTF0_4F_LeftDown_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-5_horizontal" };
-        public ObjectiveTestItem MTF0_4F_LeftDown_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-5_Vertical" };
+        public ObjectiveTestItem MTF0_4F_LeftDown_H1 { get; set; } = new ObjectiveTestItem() { Name = "P5_H1" };
+        public ObjectiveTestItem MTF0_4F_LeftDown_V1 { get; set; } = new ObjectiveTestItem() { Name = "P5_V1" };
+        public ObjectiveTestItem MTF0_4F_LeftDown_H2 { get; set; } = new ObjectiveTestItem() { Name = "P5_H2" };
+        public ObjectiveTestItem MTF0_4F_LeftDown_V2 { get; set; } = new ObjectiveTestItem() { Name = "P5_V2" };
+        public ObjectiveTestItem MTF0_4F_LeftDown_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P5_horizontal" };
+        public ObjectiveTestItem MTF0_4F_LeftDown_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P5_Vertical" };
 
 
         // 0.8F LeftUp
-        public ObjectiveTestItem MTF0_8F_LeftUp_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-6_H1" };
-        public ObjectiveTestItem MTF0_8F_LeftUp_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-6_V1" };
-        public ObjectiveTestItem MTF0_8F_LeftUp_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-6_H2" };
-        public ObjectiveTestItem MTF0_8F_LeftUp_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-6_V2" };
-        public ObjectiveTestItem MTF0_8F_LeftUp_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-6_horizontal" };
-        public ObjectiveTestItem MTF0_8F_LeftUp_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-6_Vertical" };
+        public ObjectiveTestItem MTF0_8F_LeftUp_H1 { get; set; } = new ObjectiveTestItem() { Name = "P6_H1" };
+        public ObjectiveTestItem MTF0_8F_LeftUp_V1 { get; set; } = new ObjectiveTestItem() { Name = "P6_V1" };
+        public ObjectiveTestItem MTF0_8F_LeftUp_H2 { get; set; } = new ObjectiveTestItem() { Name = "P6_H2" };
+        public ObjectiveTestItem MTF0_8F_LeftUp_V2 { get; set; } = new ObjectiveTestItem() { Name = "P6_V2" };
+        public ObjectiveTestItem MTF0_8F_LeftUp_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P6_horizontal" };
+        public ObjectiveTestItem MTF0_8F_LeftUp_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P6_Vertical" };
 
         // 0.8F RightUp
-        public ObjectiveTestItem MTF0_8F_RightUp_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-7_H1" };
-        public ObjectiveTestItem MTF0_8F_RightUp_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-7_V1" };
-        public ObjectiveTestItem MTF0_8F_RightUp_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-7_H2" };
-        public ObjectiveTestItem MTF0_8F_RightUp_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-7_V2" };
-        public ObjectiveTestItem MTF0_8F_RightUp_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-7_horizontal" };
-        public ObjectiveTestItem MTF0_8F_RightUp_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-7_Vertical" };
+        public ObjectiveTestItem MTF0_8F_RightUp_H1 { get; set; } = new ObjectiveTestItem() { Name = "P7_H1" };
+        public ObjectiveTestItem MTF0_8F_RightUp_V1 { get; set; } = new ObjectiveTestItem() { Name = "P7_V1" };
+        public ObjectiveTestItem MTF0_8F_RightUp_H2 { get; set; } = new ObjectiveTestItem() { Name = "P7_H2" };
+        public ObjectiveTestItem MTF0_8F_RightUp_V2 { get; set; } = new ObjectiveTestItem() { Name = "P7_V2" };
+        public ObjectiveTestItem MTF0_8F_RightUp_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P7_horizontal" };
+        public ObjectiveTestItem MTF0_8F_RightUp_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P7_Vertical" };
 
         // 0.8F RightDown
-        public ObjectiveTestItem MTF0_8F_RightDown_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-8_H1" };
-        public ObjectiveTestItem MTF0_8F_RightDown_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-8_V1" };
-        public ObjectiveTestItem MTF0_8F_RightDown_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-8_H2" };
-        public ObjectiveTestItem MTF0_8F_RightDown_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-8_V2" };
-        public ObjectiveTestItem MTF0_8F_RightDown_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-8_horizontal" };
-        public ObjectiveTestItem MTF0_8F_RightDown_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-8_Vertical" };
+        public ObjectiveTestItem MTF0_8F_RightDown_H1 { get; set; } = new ObjectiveTestItem() { Name = "P8_H1" };
+        public ObjectiveTestItem MTF0_8F_RightDown_V1 { get; set; } = new ObjectiveTestItem() { Name = "P8_V1" };
+        public ObjectiveTestItem MTF0_8F_RightDown_H2 { get; set; } = new ObjectiveTestItem() { Name = "P8_H2" };
+        public ObjectiveTestItem MTF0_8F_RightDown_V2 { get; set; } = new ObjectiveTestItem() { Name = "P8_V2" };
+        public ObjectiveTestItem MTF0_8F_RightDown_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P8_horizontal" };
+        public ObjectiveTestItem MTF0_8F_RightDown_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P8_Vertical" };
 
         // 0.8F LeftDown
-        public ObjectiveTestItem MTF0_8F_LeftDown_H1 { get; set; } = new ObjectiveTestItem() { Name = "Point-9_H1" };
-        public ObjectiveTestItem MTF0_8F_LeftDown_V1 { get; set; } = new ObjectiveTestItem() { Name = "Point-9_V1" };
-        public ObjectiveTestItem MTF0_8F_LeftDown_H2 { get; set; } = new ObjectiveTestItem() { Name = "Point-9_H2" };
-        public ObjectiveTestItem MTF0_8F_LeftDown_V2 { get; set; } = new ObjectiveTestItem() { Name = "Point-9_V2" };
-        public ObjectiveTestItem MTF0_8F_LeftDown_horizontal { get; set; } = new ObjectiveTestItem() { Name = "Point-9_horizontal" };
-        public ObjectiveTestItem MTF0_8F_LeftDown_Vertical { get; set; } = new ObjectiveTestItem() { Name = "Point-9_Vertical" };
+        public ObjectiveTestItem MTF0_8F_LeftDown_H1 { get; set; } = new ObjectiveTestItem() { Name = "P9_H1" };
+        public ObjectiveTestItem MTF0_8F_LeftDown_V1 { get; set; } = new ObjectiveTestItem() { Name = "P9_V1" };
+        public ObjectiveTestItem MTF0_8F_LeftDown_H2 { get; set; } = new ObjectiveTestItem() { Name = "P9_H2" };
+        public ObjectiveTestItem MTF0_8F_LeftDown_V2 { get; set; } = new ObjectiveTestItem() { Name = "P9_V2" };
+        public ObjectiveTestItem MTF0_8F_LeftDown_horizontal { get; set; } = new ObjectiveTestItem() { Name = "P9_horizontal" };
+        public ObjectiveTestItem MTF0_8F_LeftDown_Vertical { get; set; } = new ObjectiveTestItem() { Name = "P9_Vertical" };
 
     }
 }
