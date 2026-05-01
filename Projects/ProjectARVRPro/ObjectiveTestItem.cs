5c24b2b5d4df0b99cfb2509ef27484e8dd866885
xincheng <xincheng213618@gmail.com>
2026-05-02 02:40:50 +0800

    update

---
+++ Projects/ProjectARVRPro/ObjectiveTestItem.cs
@@ -0,0 +1,31 @@
+﻿namespace ProjectARVRPro
+{
+    public class ObjectiveTestItem
+    {
+        public string Name { get; set; }         // 项目名称
+
+        //这里有可能添加符号
+        public string TestValue { get; set; }    // 测试值
+
+        public double Value { get; set; }    // 测试值
+
+        public double LowLimit { get; set; }     // 下限
+        public double UpLimit { get; set; }      // 上限
+
+        public string Unit { get; set; }         // 单位
+
+        public bool TestResult {
+            get
+            {
+                // 判断是否低于下限
+                bool isAboveLowLimit = LowLimit == 0 || Value >= LowLimit;
+                // 判断是否高于上限
+                bool isBelowUpLimit = UpLimit == 0 || Value <= UpLimit;
+                // 只有同时满足上下限才返回 true
+                return isAboveLowLimit && isBelowUpLimit;
+            } 
+        }
+    }
+
+
+}
