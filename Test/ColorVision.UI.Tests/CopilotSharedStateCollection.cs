27fa00db9889c9cf6582fecd033b6b33417e9d14
xincheng <xincheng213618@gmail.com>
2026-07-16 18:55:01 +0800

    test(copilot): remove regression suite

--- Test/ColorVision.UI.Tests/CopilotSharedStateCollection.cs
+++
@@ -1,7 +0,0 @@
-namespace ColorVision.UI.Tests;
-
-[CollectionDefinition(Name, DisableParallelization = true)]
-public sealed class CopilotSharedStateTestGroup
-{
-    public const string Name = "Copilot shared state";
-}
