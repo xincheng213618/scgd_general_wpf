1a3b7eaca3de1eeec7e76821c4f6ea6227308338
xincheng <xincheng213618@gmail.com>
2026-07-21 12:11:28 +0800

    1

---
+++ Engine/ColorVision.Engine/Services/Devices/Camera/Video/CameraVideoConfig.cs
@@ -0,0 +1,74 @@
+﻿using ColorVision.Common.MVVM;
+using System;
+using System.ComponentModel;
+
+
+namespace ColorVision.Engine.Services.Devices.Camera.Video
+{
+    public class CameraVideoConfig : ViewModelBase
+    {
+        public CameraVideoConfig()
+        {
+            DeafutPort++;
+            _Port = Common.Utilities.Tool.GetFreePort(DeafutPort);
+        }
+        [DisplayName("主机")]
+        public string Host { get => _Host; set { _Host = value; NotifyPropertyChanged(); } }
+
+        private void NotifyPropertyChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string _Host = "127.0.0.1";
+
+        private static int DeafutPort { get; set; } = 9002;
+
+
+        /// <summary>
+        /// 端口地址
+        /// </summary>
+        [DisplayName("端口地址")]
+        public int Port
+        {
+            get => _Port; set
+            {
+                _Port = value <= 0 ? 0 : value >= 65535 ? 65535 : value;
+
+                NotifyPropertyChanged();
+            }
+        }
+        private int _Port;
+
+        [Browsable(false)]
+        public long Capacity
+        {
+            get => _Capacity;
+            set
+            {
+                if (_Capacity != value)
+                {
+                    _Capacity = value;
+                    NotifyPropertyChanged();
+                    NotifyPropertyChanged(nameof(CapacityText));
+                }
+            }
+        }
+        private long _Capacity = 1073741824;
+
+        [JsonIgnore]
+        [DisplayName("缓存大小")]
+        public string CapacityText
+        {
+            get => Common.Utilities.MemorySize.MemorySizeText(_Capacity);
+            set
+            {
+                if (Common.Utilities.MemorySize.TryParseMemorySize(value, out long parsedValue))
+                {
+                    Capacity = parsedValue;
+                }
+            }
+        }
+
+    }
+}
