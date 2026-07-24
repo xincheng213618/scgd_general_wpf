3b5afe8780b480546ddd81256de7c862157cb274
xincheng <xincheng213618@gmail.com>
2026-07-24 18:33:52 +0800

    update

--- Engine/ColorVision.Engine/EngineCommands.cs
+++ Engine/ColorVision.Engine/EngineCommands.cs
@@ -2,51 +2,8 @@
 
 namespace ColorVision.Engine
 {
-
-    //public class MenuEngine : GlobalMenuBase
-    //{
-    //    public override string GuidId => EngineCommands.EngineGuidId;
-    //    public override string Header => ColorVision.Engine.Properties.Resources.MenuEngine;
-    //    public override int Order => 2;
-    //}
-    //public class MenuStartExecutionCommand : MenuItemBase
-    //{
-    //    public override string OwnerGuid => EngineCommands.EngineGuidId;
-    //    public override string Header => ColorVision.Engine.Properties.Resources.StartExecition;
-    //    public override int Order => 99;
-
-    //    public override ICommand Command => EngineCommands.StartExecutionCommand;
-    //    public override object? Icon => MenuItemIcon.TryFindResource("DIRun");
-
-    //    public override string InputGestureText => "F6";
-    //}
-
-    //public class MenuStopExecutionCommand : MenuItemBase
-    //{
-    //    public override string OwnerGuid => EngineCommands.EngineGuidId;
-    //    public override string Header => ColorVision.Engine.Properties.Resources.StopExecution;
-    //    public override int Order => 99;
-    //    public override ICommand Command => EngineCommands.StopExecutionCommand;
-    //    public override object? Icon => MenuItemIcon.TryFindResource("DIRunPaused");
-    //    public override string InputGestureText => "F7";
-    //}
-
-    //public class MenuTakePhotoCommand : MenuItemBase
-    //{
-    //    public override string OwnerGuid => EngineCommands.EngineGuidId;
-
-    //    public override string Header => ColorVision.Engine.Properties.Resources.CaptureImage;
-    //    public override int Order => 99;
-
-    //    public override ICommand Command => EngineCommands.TakePhotoCommand;
-    //    public override string InputGestureText => "Ctrl+T";
-    //}
-
-
     public static class EngineCommands
     {
-        public static string EngineGuidId => "Engine";
-
         /// <summary>
         /// 开始执行
         /// </summary>
